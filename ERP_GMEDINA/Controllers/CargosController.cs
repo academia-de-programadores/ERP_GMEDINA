b902b439b653f2719022d0525c8c9a26d8c8ba5b
LuisR-FF <57548868+LuisR-FF@users.noreply.github.com>
2019-11-14 08:36:57 -0600

    Modificado 14/11/2019 8:37 am
    Creacion del controlador de la tabla cargos

---
+++ ERP_GMEDINA/Controllers/CargosController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ERP_GMEDINA.Models;
+
+namespace ERP_GMEDINA.Controllers
+{
+    public class CargosController : Controller
+    {
+        private ERP_GMEDINAEntities db = new ERP_GMEDINAEntities();
+
+        // GET: Cargos
+        public ActionResult Index()
+        {
+            var tbCargos = db.tbCargos.Include(t => t.tbUsuario).Include(t => t.tbUsuario1);
+            return View(tbCargos.ToList());
+        }
+
+        // GET: Cargos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbCargos tbCargos = db.tbCargos.Find(id);
+            if (tbCargos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbCargos);
+        }
+
+        // GET: Cargos/Create
+        public ActionResult Create()
+        {
+            ViewBag.car_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario");
+            ViewBag.car_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario");
+            return View();
+        }
+
+        // POST: Cargos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "car_Id,car_Descripcion,car_Estado,car_RazonInactivo,car_UsuarioCrea,car_FechaCrea,car_UsuarioModifica,car_FechaModifica")] tbCargos tbCargos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.tbCargos.Add(tbCargos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.car_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbCargos.car_UsuarioCrea);
+            ViewBag.car_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbCargos.car_UsuarioModifica);
+            return View(tbCargos);
+        }
+
+        // GET: Cargos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbCargos tbCargos = db.tbCargos.Find(id);
+            if (tbCargos == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.car_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbCargos.car_UsuarioCrea);
+            ViewBag.car_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbCargos.car_UsuarioModifica);
+            return View(tbCargos);
+        }
+
+        // POST: Cargos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "car_Id,car_Descripcion,car_Estado,car_RazonInactivo,car_UsuarioCrea,car_FechaCrea,car_UsuarioModifica,car_FechaModifica")] tbCargos tbCargos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tbCargos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.car_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbCargos.car_UsuarioCrea);
+            ViewBag.car_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbCargos.car_UsuarioModifica);
+            return View(tbCargos);
+        }
+
+        // GET: Cargos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbCargos tbCargos = db.tbCargos.Find(id);
+            if (tbCargos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tbCargos);
+        }
+
+        // POST: Cargos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            tbCargos tbCargos = db.tbCargos.Find(id);
+            db.tbCargos.Remove(tbCargos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
