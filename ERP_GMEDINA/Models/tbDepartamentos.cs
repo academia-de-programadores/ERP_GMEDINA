bba0803079aed2cc8acdf2e53305b55ee1b69d4e
Enamorado8 <53063416+Enamorado8@users.noreply.github.com>
2019-11-14 20:32:42 -0600

    Actualizacion Modelo
    Eliminacion tabla tbRequisicionPersonsal

--- ERP_GMEDINA/Models/tbDepartamentos.cs
+++ ERP_GMEDINA/Models/tbDepartamentos.cs
@@ -19,7 +19,6 @@ namespace ERP_GMEDINA.Models
         {
             this.tbEmpleados = new HashSet<tbEmpleados>();
             this.tbHistorialContrataciones = new HashSet<tbHistorialContrataciones>();
-            this.tbRequisicionPersonal = new HashSet<tbRequisicionPersonal>();
         }
     
         public int depto_Id { get; set; }
@@ -41,7 +40,5 @@ namespace ERP_GMEDINA.Models
         public virtual ICollection<tbEmpleados> tbEmpleados { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<tbHistorialContrataciones> tbHistorialContrataciones { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<tbRequisicionPersonal> tbRequisicionPersonal { get; set; }
     }
 }
