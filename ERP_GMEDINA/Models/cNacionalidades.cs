a111782cbf7c053d92311ece64a0e765b3c8dc23
Sisy Paz <53066659+SisyPaz@users.noreply.github.com>
2019-11-15 11:11:53 -0600

    Avance Controller y Views
    Avance Controller y Views Nacionalidades

---
+++ ERP_GMEDINA/Models/cNacionalidades.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations; //Librería que se agrega
+using System.Linq;
+using System.Web;
+
+
+namespace ERP_GMEDINA.Models
+{
+    [MetadataType(typeof(cNacionalidades))]
+
+    public partial class tbNacionalidades
+    {
+
+    }
+
+    public class cNacionalidades
+    {
+        [Display(Name = "ID")]
+        public int nac_Id { get; set; }
+
+        [Display(Name = "Descripción")]
+        public string nac_Descripcion { get; set; }
+
+        [Display(Name = "Estado")]
+        public bool nac_Estado { get; set; }
+
+        [Display(Name = "Razón Inactivo")]
+        public string nac_RazonInactivo { get; set; }
+
+        [Display(Name = "Usuario Crea")]
+        public int nac_UsuarioCrea { get; set; }
+
+        [Display(Name = "Fecha Crea")]
+        public System.DateTime nac_FechaCrea { get; set; }
+
+        [Display(Name = "Usuario Modifica")]
+        public Nullable<int> nac_UsuarioModifica { get; set; }
+
+        [Display(Name = "Fecha Modifica")]
+        public Nullable<System.DateTime> nac_FechaModifica { get; set; }
+    }
+}
