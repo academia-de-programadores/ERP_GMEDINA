90dad96f54bde31b1ff5e876f54fb1d241ae23d5
Carlos Flamenco <57505181+cflamenco99@users.noreply.github.com>
2019-11-14 23:37:42 -0600

    datatble buttons

--- ERP_GMEDINA/Scripts/plugins/dataTables/DatatablesConfigurations.js
+++ ERP_GMEDINA/Scripts/plugins/dataTables/DatatablesConfigurations.js
@@ -5,22 +5,61 @@
         pageLength: 25,
         dom: '<"html5buttons"B>lTfgitp',
         buttons: [
-            { extend: 'copy' },
-            { extend: 'csv' },
-            { extend: 'excel', title: 'ExampleFile' },
-            { extend: 'pdf', title: 'ExampleFile' },
+            {
+                extend: 'copy',
+                text: '<i class="fa fa-copy btn-xs"></i>',
+                titleAttr: 'Copiar',
+                exportOptions: {
+                    columns: [0, 1, 2, 3, 4],
+                },
+                className: 'btn btn-primary'
 
+            },
+
+            {
+                extend: 'excel',
+                text: '<i class="fa fa-file-excel-o btn-xs"></i>',
+                titleAttr: 'Excel',
+                exportOptions: {
+                    columns: [0, 1, 2, 3, 4],
+                },
+                className: 'btn btn-primary',
+                title: 'Excel'
+            },
+            {
+                extend: 'pdf',
+                text: '<i class="fa fa-file-pdf-o btn-xs"></i>',
+                titleAttr: 'PDF',
+                exportOptions: {
+                    columns: [0, 1, 2, 3, 4],
+                },
+                className: 'btn btn-primary',
+                title: 'PDF'
+            },
             {
                 extend: 'print',
                 customize: function (win) {
-                    $(win.document.body).addClass('white-bg');
-                    $(win.document.body).css('font-size', '10px');
+                    $(win.document.body)
+                        .css('font-size', '10pt')
+                        .prepend($('<img />')
+                            .attr('src', 'Hospital_BI/assets/img/bi-stroke.png')
+                            .addClass('asset-print-img')
+                        );
 
                     $(win.document.body).find('table')
-                            .addClass('compact')
-                            .css('font-size', 'inherit');
-                }
+                        .addClass('compact')
+                        .css('font-size', 'inherit');
+                },
+                messageTop: 'Reporte',
+
+                exportOptions: {
+                    columns: [0, 1, 2, 3, 4],
+                },
+                className: 'btn btn-primary',
+                text: '<i class="fa fa-print btn-xs"></i>',
+                titleAttr: 'Imprimir'
             }
+
         ]
     });
 });
