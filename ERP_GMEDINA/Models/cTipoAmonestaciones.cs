383728d2d906ababb8d3b562a9cecfb569e63eed
Isis10 <53128197+Isis10@users.noreply.github.com>
2019-11-15 07:51:26 -0600

    views
    create, details, Edit

--- ERP_GMEDINA/Models/cTipoAmonestaciones.cs
+++ ERP_GMEDINA/Models/cTipoAmonestaciones.cs
@@ -13,10 +13,10 @@ namespace ERP_GMEDINA.Models
     }
     public class cTipoAmonestaciones
     {
-        [Display(Name ="ID")]
+        [Display(Name ="ID ")]
         public int tamo_Id { get; set; }
 
-        [Display (Name ="Descripcion Amonestacion")]
+        [Display (Name ="Descripcion")]
         public string tamo_Descripcion { get; set; }
 
         [Display(Name ="Estado")]
@@ -27,6 +27,15 @@ namespace ERP_GMEDINA.Models
 
         [Display(Name = "Usuario Crea")]
         public int tamo_UsuarioCrea { get; set; }
+
+        [Display(Name = "Fecha Crea")]
+        public System.DateTime tamo_FechaCrea { get; set; }
+
+        [Display(Name = "Usuario Modifica")]
+        public Nullable<int> tamo_UsuarioModifica { get; set; }
+
+        [Display(Name = "Fecha Modifica")]
+        public Nullable<System.DateTime> tamo_FechaModifica { get; set; }
 
     }
 }
