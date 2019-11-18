0220b31e0712c39a9ba78a383833d7ddff9c06f3
keyci1113 <47374214+keyci1113@users.noreply.github.com>
2019-11-18 11:06:21 -0600

    sss

--- ERP_GMEDINA/Scripts/app/General/TipoHoras/TipoHoras.js
+++ ERP_GMEDINA/Scripts/app/General/TipoHoras/TipoHoras.js
@@ -184,6 +184,7 @@ function tablaDetalle(ID) {
     // $(document).on("click", "#IndexTable tbody tr td #btnDetalle", function () {
     //var tr = this.closest("tr");
     id = ID;
+    var ide = id;
     //var id = $(this).closest('tr').data('id');
     $.ajax({
         url: "/TipoHoras/Details/" + id,
@@ -202,7 +203,7 @@ function tablaDetalle(ID) {
                 $("#ModalDetallesR").find("#tiho_FechaModifica")["0"].innerText = FechaFormato(data.tiho_FechaModifica);
                 $("#ModalDetallesR").find("#tbUsuario_usu_NombreUsuario")["0"].innerText = data.tbUsuario.usu_NombreUsuario;
                 $("#ModalDetallesR").find("#tbUsuario1_usu_NombreUsuario")["0"].innerText = data.tbUsuario1.usu_NombreUsuario;
-                $("#ModalDetallesR").find("#btnEditarM")["0"].dataset.id = id;
+                $("#ModalDetallesR").find("#btnEditarM")["0"].dataset.id = ide;
                 $('#ModalDetallesR').modal('show');
             }
             else {
