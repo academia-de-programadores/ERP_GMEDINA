e0025c257019fd7ee5548653336075e7068d1bdd
EG0782 <53062171+EG0782@users.noreply.github.com>
2019-11-18 07:20:22 -0600

    18/11/2019 ModalEdit(Test1)

--- ERP_GMEDINA/Scripts/app/General/Empresas/Empresas.js
+++ ERP_GMEDINA/Scripts/app/General/Empresas/Empresas.js
@@ -1,10 +1,10 @@
 ﻿
 //----------------------------------------------------------  Mostrar modal "Crear" e "UDP Insert" -----------------------------------------------------------------------------
-
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // Mostrar modal
 $(document).on("click", "#btnAgregarEmpresas", function () {
     //MOSTRAR EL MODAL DE AGREGAR
-    $("#ModalCrear").modal();
+        
 });
 
 // Insert 
@@ -40,10 +40,77 @@ $('#btnAgregar').click(function () {
 });
 
 //----------------------------------------------------------  Mostrar modal "Editar" y "UDP Update" -----------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Mostrar modal con info
+//$(document).on("click", "#IndexTable tbody tr td #btnEditarEmpresas", function () {
+//    var ID = $(this).closest('tr').data('id');
+//    console.log(ID);
 
-//-------------------------------------------------------------------------------------------------------------------------------------------
+//    $.ajax({
+//        url: "/Empresas/Edit/" + ID,
+//        method: "GET",
+//        dataType: "json",
+//        contentType: "application/json; charset=utf-8",
+//        data: JSON.stringify({ ID: ID })
+//    })
+//        .done(function (data) {
+//            //SI SE OBTIENE DATA, LLENAR LOS CAMPOS DEL MODAL CON ELLA
+//            if (data.length > 0) {
+//                console.log('Hla')
+//                console.log(data)
+//                $.each(data, function (i, item) {
+//                    $("#ModalEditar #empr_Id").val(item.empr_Id);
+//                    $("#ModalEditar #empr_Nombre").val(item.empr_Nombre);
+//                    $("#ModalEditar #empr_RazonInactivo").val(item.empr_RazonInactivo);
+//                })
+
+                
+//            }
+//            else {
+//                //Mensaje de error si no hay data
+//                iziToast.error({
+//                    title: 'Error',
+//                    message: 'No se pudo cargar la información, contacte al administrador',
+//                });
+//            }
+
+//            $("#ModalEditar").modal();
+//        });
+//});
+
+$(document).on("click", "#IndexTable tbody tr td #btnEditarEmpresas", function () {
+    var id = $(this).closest('tr').data('id');
+    console.log(id);
+    $.ajax({
+        url: "/Empresas/Edit/" + id,
+        method: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({ id: id })
+    })
+        .done(function (data) {
+            //SI SE OBTIENE DATA, LLENAR LOS CAMPOS DEL MODAL CON ELLA
+
+            if (data.length > 0) {
+                console.log("funciona");
+                console.log(data);
+                $.each(data, function (i, item) {
+                    $("#ModalEdit #empr_Id").val(item.empr_Id);
+                    $("#ModalEdit #empr_Nombre").val(item.empr_Nombre);
+                    $("#ModalEdit #empr_RazonInactivo").val(item.empr_RazonInactivo);
+                    //$("#ModalEdit #tiho_UsuarioCrea").val(item.tiho_UsuarioCrea)
+                    //$("#ModalEdit #tiho_FechaCrea").val(item.tiho_FechaCrea);
+                })
+                $("#ModalEditar").modal();
+            }
+        })
+});
 
 
 
-//----------------------------------------------------------  Mostrar modal "Deshabilitar" y "UDP Delete" -----------------------------------------------------------------------------
-//-------------------------------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------  Mostrar modal "Deshabilitar" y "UDP Delete" -----------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+//----------------------------------------------------------  Otras funciones ---------------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
