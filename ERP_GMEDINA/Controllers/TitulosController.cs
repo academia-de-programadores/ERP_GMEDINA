f77fa7d709b0693dee2b5831bd72ff64b39bfa4f
carol2198 <53068732+carol2198@users.noreply.github.com>
2019-11-18 22:02:23 -0600

    titulos

--- ERP_GMEDINA/Controllers/TitulosController.cs
+++ ERP_GMEDINA/Controllers/TitulosController.cs
@@ -21,8 +21,12 @@ namespace ERP_GMEDINA.Controllers
             return View(tbTitulos.ToList());
         }
 
+
+
+
+
         // GET: Titulos/Details/5
-        public ActionResult Details(int? id)
+        /*public ActionResult Details(int? id)
         {
             if (id == null)
             {
@@ -35,33 +39,62 @@ namespace ERP_GMEDINA.Controllers
             }
             return View(tbTitulos);
         }
+        */
+
 
         // GET: Titulos/Create
-        public ActionResult Create()
-        {
-            ViewBag.titu_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario");
-            ViewBag.titu_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario");
-            return View();
-        }
+    
 
         // POST: Titulos/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "titu_Id,titu_Descripcion,titu_Estado,titu_RazonInactivo,titu_UsuarioCrea,titu_FechaCrea,titu_UsuarioModifica,titu_FechaModifica")] tbTitulos tbTitulos)
+        public ActionResult Create([Bind(Include = "titu_Id,titu_Descripcion,titu_Estado,titu_RazonInactivo,titu_UsuarioCrea,titu_FechaCrea,titu_UsuarioModifica,titu_FechaModifica")] tbTitulos tbtitulos)
         {
+            tbtitulos.titu_UsuarioCrea = 1;
+            tbtitulos.titu_FechaCrea = DateTime.Now;
+
+            string response = String.Empty;
+            IEnumerable<object> listatitulos = null;
+            string MensajeError = "";
             if (ModelState.IsValid)
             {
-                db.tbTitulos.Add(tbTitulos);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                try
+                {
+                    listatitulos = db.UDP_RRHH_tbTitulos_Insert(tbtitulos.titu_Descripcion,
+                                                                tbtitulos.titu_UsuarioCrea,
+                                                                tbtitulos.titu_FechaCrea
+                                                                );
+                    foreach (UDP_RRHH_tbTitulos_Insert_Result Resultado in listatitulos)
+                        MensajeError = Resultado.MensajeError;
+
+                    if (MensajeError.StartsWith("-1"))
+                    {
+                        ModelState.AddModelError("", "No se pudo ingresar el registro");
+                        response = "error";
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    response = ex.Message.ToString();
+                }
+                response = "bien";
+
+            }
+            else
+            {
+                response = "error";
             }
 
-            ViewBag.titu_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTitulos.titu_UsuarioCrea);
-            ViewBag.titu_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTitulos.titu_UsuarioModifica);
-            return View(tbTitulos);
+
+            ViewBag.titu_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbtitulos.titu_UsuarioCrea);
+            ViewBag.titu_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbtitulos.titu_UsuarioModifica);
+            return Json(response,JsonRequestBehavior.AllowGet);
         }
+
+
 
         // GET: Titulos/Edit/5
         public ActionResult Edit(int? id)
@@ -80,22 +113,57 @@ namespace ERP_GMEDINA.Controllers
             return View(tbTitulos);
         }
 
+
+
         // POST: Titulos/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "titu_Id,titu_Descripcion,titu_Estado,titu_RazonInactivo,titu_UsuarioCrea,titu_FechaCrea,titu_UsuarioModifica,titu_FechaModifica")] tbTitulos tbTitulos)
+        public ActionResult Edit([Bind(Include = "titu_Id,titu_Descripcion,titu_Estado,titu_RazonInactivo,titu_UsuarioCrea,titu_FechaCrea,titu_UsuarioModifica,titu_FechaModifica")] tbTitulos tbtitulos)
         {
+            tbtitulos.titu_UsuarioCrea = 1;
+            tbtitulos.titu_FechaCrea = DateTime.Now;
+
+            tbtitulos.titu_UsuarioModifica = 1;
+            tbtitulos.titu_FechaModifica = DateTime.Now;
+
+            string response = String.Empty;
+            IEnumerable<object> listatitulos = null;
+            string MensajeError = "";
+
             if (ModelState.IsValid)
             {
-                db.Entry(tbTitulos).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                try
+                {
+                    listatitulos = db.UDP_RRHH_tbTitulos_Update(tbtitulos.titu_Id,
+                                                                tbtitulos.titu_Descripcion,
+                                                                tbtitulos.titu_UsuarioModifica,
+                                                                tbtitulos.titu_FechaModifica
+                                                                );
+                    foreach (UDP_RRHH_tbTitulos_Update_Result Resultado in listatitulos)
+                        MensajeError = Resultado.MensajeError;
+
+                    if (MensajeError.StartsWith("-1"))
+                    {
+                        ModelState.AddModelError("", "No se pudo ingresar el registron,conecte al administrador");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    response = ex.Message.ToString();
+                }
+                response = "bien";
+
+            } 
+            else
+            {
+                ModelState.AddModelError("", "no se pudo modificar el registro, conecte con el administrador");
+                response = "error";
             }
-            ViewBag.titu_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTitulos.titu_UsuarioCrea);
-            ViewBag.titu_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTitulos.titu_UsuarioModifica);
-            return View(tbTitulos);
+            ViewBag.titu_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbtitulos.titu_UsuarioCrea);
+            ViewBag.titu_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbtitulos.titu_UsuarioModifica);
+            return Json(response, JsonRequestBehavior.AllowGet);
         }
 
         // GET: Titulos/Delete/5
