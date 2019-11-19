6508597af26f8266e7d753c2839548511256483d
Kevin Aleman <kev4leman@hotmail.com>
2019-11-19 11:33:52 -0600

    Modal Edit
    En proceso.

--- ERP_GMEDINA/Scripts/app/General/TipoMonedas/TipoMonedas.js
+++ ERP_GMEDINA/Scripts/app/General/TipoMonedas/TipoMonedas.js
@@ -11,7 +11,7 @@ $('#btnAgregar').click(function () {
     // SIEMPRE HACER LAS RESPECTIVAS VALIDACIONES DEL LADO DEL CLIENTE
 
     //SERIALIZAR EL FORMULARIO DEL MODAL (ESTÁ EN LA VISTA PARCIAL)
-    var data = $("#fmrTipoMonedasCreate").serializeArray();
+    var data = $("#frmTipoMonedasCreate").serializeArray();
     //ENVIAR DATA AL SERVIDOR PARA EJECUTAR LA INSERCIÓN
     $.ajax({
         url: "/TipoMonedas/Create",
@@ -36,4 +36,35 @@ $('#btnAgregar').click(function () {
             });
         }
     });
+});
+
+//FUNCION: PRIMERA FASE DE EDICION DE REGISTROS, MOSTRAR MODAL CON LA INFORMACIÓN DEL REGISTRO SELECCIONADO
+$(document).on("click", "#tblTipoMonedas tbody tr td #btnEditarTipoMonedas", function () {
+    var ID = $(this).data('id');
+    $.ajax({
+        url: "/TipoMonedas/Edit/" + ID,
+        method: "GET",
+        dataType: "json",
+        contentType: "application/json; charset=utf-8",
+        data: JSON.stringify({ ID: ID })
+    })
+        .done(function (data) {
+            //SI SE OBTIENE DATA, LLENAR LOS CAMPOS DEL MODAL CON ELLA
+            if (data) {
+                console.log('Hla')
+                $("#Editar #cde_IdDeducciones").val(data.cde_IdDeducciones);
+                $("#Editar #cde_DescripcionDeduccion").val(data.cde_DescripcionDeduccion);
+                $("#Editar #cde_PorcentajeColaborador").val(data.cde_PorcentajeColaborador);
+                $("#Editar #cde_PorcentajeEmpresa").val(data.cde_PorcentajeEmpresa);
+             
+                $("#EditarCatalogoDeducciones").modal();
+            }
+            else {
+                //Mensaje de error si no hay data
+                iziToast.error({
+                    title: 'Error',
+                    message: 'No se pudo cargar la información, contacte al administrador',
+                });
+            }
+        });
 });
