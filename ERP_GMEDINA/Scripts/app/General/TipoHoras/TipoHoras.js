770c990251458ed43bf2633bf06f688abd2b8a76
keyci1113 <47374214+keyci1113@users.noreply.github.com>
2019-11-19 16:11:39 -0600

    stuff
    stuff

--- ERP_GMEDINA/Scripts/app/General/TipoHoras/TipoHoras.js
+++ ERP_GMEDINA/Scripts/app/General/TipoHoras/TipoHoras.js
@@ -14,59 +14,48 @@ function serializar(data) {
 ///FUNCION SERIALZAR
 
 
-function GEEKFORGEEKS()								 
-{ 
-    var name = document.forms["RegForm"]["Name"];			 
-    var email = document.forms["RegForm"]["EMail"]; 
-    var phone = document.forms["RegForm"]["Telephone"]; 
-    var what = document.forms["RegForm"]["Subject"]; 
-    var password = document.forms["RegForm"]["Password"]; 
-    var address = document.forms["RegForm"]["Address"]; 
+//function VALIDAR(Modal)								 
+//{ 
+//    var Des = document.forms[Modal]["tiho_Descripcion"];
+//    var Recargo = document.forms[Modal]["tiho_Recargo"];
+//    var RazonActivo = document.forms[Modal]["tiho_RazonInactivo"];
 
-    if (name.value == "")								 
-    { 
-        window.alert("Please enter your name."); 
-        name.focus(); 
-        return false; 
-    } 
 
-    if (address.value == "")							 
-    { 
-        window.alert("Please enter your address."); 
-        address.focus(); 
-        return false; 
-    } 
+//    if ($.trim(Des).value == "")
+//    { 
+//        iziToast.warning({
+//            title: 'Error',
+//            message: 'Campo descripcion esta vacio',
+//        });
+//        Des.focus();
+       
+//        return false; 
+//    } 
+
+//    if ($.trim(Recargo).value == "")
+//    {
+//        iziToast.warning({
+//            title: 'Error',
+//            message: 'Campo Recargo esta vacio',
+//        });
+//        Recargo.focus();
+       
+//        return false; 
+//    } 
 	
-    if (email.value == "")								 
-    { 
-        window.alert("Please enter a valid e-mail address."); 
-        email.focus(); 
-        return false; 
-    } 
+//    if ($.trim(RazonActivo).value == "")
+//    {
+//        iziToast.warning({
+//            title: 'Error',
+//            message: 'Campo Razon Activo esta vacio',
+//        });
+//        RazonActivo.val("");
+//        return false; 
+//    } 
 
-    if (phone.value == "")						 
-    { 
-        window.alert("Please enter your telephone number."); 
-        phone.focus(); 
-        return false; 
-    } 
-
-    if (password.value == "")					 
-    { 
-        window.alert("Please enter your password"); 
-        password.focus(); 
-        return false; 
-    } 
-
-    if (what.selectedIndex < 1)				 
-    { 
-        alert("Please enter your course."); 
-        what.focus(); 
-        return false; 
-    } 
-
-    return true; 
-}
+  
+//    return true; 
+//}
 
 function AllFunctions() {
 
@@ -93,8 +82,9 @@ function AllFunctions() {
            
         //}
         //);
-       
-    
+        //var modal = $("#ModalCrear").val();
+        //VALIDAR(modal);
+
         var data = $("#frmAgregarTipoHoras").serializeArray();
         
         $.ajax({
