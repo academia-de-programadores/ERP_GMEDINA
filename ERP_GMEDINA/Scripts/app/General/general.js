bd079476ef3e326da44b7179ecd3ef5463cf2ba9
keyci1113 <47374214+keyci1113@users.noreply.github.com>
2019-11-20 19:04:26 -0600

    oijciocjio
    iosionsdin

--- ERP_GMEDINA/Scripts/app/General/general.js
+++ ERP_GMEDINA/Scripts/app/General/general.js
@@ -1,4 +1,5 @@
-var modal = ["ModalCrear", "ModalEditar", "ModalInhabilitar", "ModalDetalles"];
+//
+var modal = ["ModalNuevo", "ModalEditar", "ModalInhabilitar", "ModalDetalles"];
 function CierraPopups() {
     $.each(modal, function (index, valor) {
         $("#" + valor).modal('hide');//ocultamos el modal
@@ -24,10 +25,10 @@ function serializar(data) {
     var verificacion = true;
     $.each(data, function (index, valor) {
         var value = valor.value.trim();
-        if (value!="") {
+        if (value != "") {
             Data[valor.name] = value;
         } else {
-            SetearClases(valor.name, "error", "valid");
+            SetearClases(valor.name, "error", "valid", "val-required");
             verificacion = false;
         }
     });
@@ -55,10 +56,17 @@ function FechaFormato(pFecha) {
 function pad2(number) {
     return (number < 10 ? '0' : '') + number
 }
-function SetearClases(Id,Agregar,Remover) {
+function SetearClases(Id, Agregar, Remover, valorError) {
     modal.forEach(function (indice, value) {
-        $("#" + indice).find("#" + Id).addClass(Agregar);
-        $("#" + indice).find("#" + Id).removeClass(Remover);
+        var spam = $("#" + indice).find("#error" + Id);
+        var input = $("#" + indice).find("#" + Id);
+        if (valorError == "") {
+            spam.text(valorError);
+        } else {
+            spam.text(input.data(valorError));
+        }
+        input.addClass(Agregar);
+        input.removeClass(Remover);
     });
 }
 function LimpiarControles(Controles) {
