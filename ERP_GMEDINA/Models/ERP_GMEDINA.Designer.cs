5d8a0146e374ce437b481c0427f7e7b4fd1defff
AxelGuzman <53068443+AxelGuzman@users.noreply.github.com>
2019-11-22 08:34:15 -0600

    cambiando
    modificaciones

--- ERP_GMEDINA/Models/ERP_GMEDINA.Designer.cs
+++ ERP_GMEDINA/Models/ERP_GMEDINA.Designer.cs
@@ -1,14 +1,14 @@
-<<<<<<< HEAD
+
 ﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Guzman\Documents\GitHub\Proyecto_ERP_GMEDINA\ERP_GMEDINA\Models\ERP_GMEDINA.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
-=======
+
 ﻿// T4 code generation is enabled for model 'C:\Users\LAB02\Documents\Visual Studio 2015\Projects\PROYECTO A MAPEAR\ERP_GMEDINA\ERP_GMEDINA\Models\ERP_GMEDINA.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
->>>>>>> parent of bba0803... Actualizacion Modelo
+
 
 // Si no se ha generado ninguna clase de contexto y de entidad, puede que haya creado un modelo vacío pero
 // no haya elegido todavía la versión de Entity Framework que se va a usar. Para generar una clase de contexto y clases de entidad
