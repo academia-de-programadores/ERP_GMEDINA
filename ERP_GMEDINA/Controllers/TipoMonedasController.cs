9edb08124cd8d70e4ed5dd13e72acfaee5dc0f6a
Kevin Alemán <57551666+Kevin4leman@users.noreply.github.com>
2019-11-22 10:36:37 -0600

    Revert "Modal Edit"
    This reverts commit 42d92215385c989d70e390cc94c247547c5de46e.

--- ERP_GMEDINA/Controllers/TipoMonedasController.cs
+++ ERP_GMEDINA/Controllers/TipoMonedasController.cs
@@ -17,17 +17,8 @@ namespace ERP_GMEDINA.Controllers
         // GET: TipoMonedas
         public ActionResult Index()
         {
-            try
-            {
-                var tbTipoMonedas = db.tbTipoMonedas.Include(t => t.tbUsuario).Include(t => t.tbUsuario1);
-                return View(tbTipoMonedas.ToList());
-            }
-            catch (Exception ex)
-            {
-                ex.Message.ToString();
-                return View(new List<tbTipoMonedas> { new tbTipoMonedas {tmon_Descripcion="no hay conecion" } });
-            }
-            
+            var tbTipoMonedas = db.tbTipoMonedas.Include(t => t.tbUsuario).Include(t => t.tbUsuario1);
+            return View(tbTipoMonedas.ToList());
         }
 
         // GET: TipoMonedas/Details/5
@@ -105,63 +96,36 @@ namespace ERP_GMEDINA.Controllers
         // GET: TipoMonedas/Edit/5
         public ActionResult Edit(int? id)
         {
-            Session["id"] = id;
-            var List = db.UDP_RRHH_tbTipoMonedas_Select(id).ToList();
-
-            return Json(List, JsonRequestBehavior.AllowGet);
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            tbTipoMonedas tbTipoMonedas = db.tbTipoMonedas.Find(id);
+            if (tbTipoMonedas == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.tmon_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTipoMonedas.tmon_UsuarioCrea);
+            ViewBag.tmon_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTipoMonedas.tmon_UsuarioModifica);
+            return View(tbTipoMonedas);
         }
 
         // POST: TipoMonedas/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        //[ValidateAntiForgeryToken]
-        public ActionResult Edit(int tmon_Id, string tmon_Descripcion)
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "tmon_Id,tmon_Descripcion,tmon_Estado,tmon_RazonInactivo,tmon_UsuarioCrea,tmon_FechaCrea,tmon_UsuarioModifica,tmon_FechaModifica")] tbTipoMonedas tbTipoMonedas)
         {
-            tbTipoMonedas TipoMoneda = new tbTipoMonedas();
-            var Usuario = (tbUsuario)Session["Usuario"];
-            TipoMoneda.tmon_Id = tmon_Id;
-            Session["TipoMoneda"] = TipoMoneda;
-            //var id = (int)Session["id"];
-            TipoMoneda.tmon_Id = tmon_Id;
-            TipoMoneda.tmon_Descripcion = tmon_Descripcion;
-
             if (ModelState.IsValid)
             {
-                string MensajeError = "";
-                try
-                {
-                    IEnumerable<object> listTipoMonedas = null;
-                    listTipoMonedas = db.UDP_RRHH_tbTipoMoneda_Update(TipoMoneda.tmon_Id,
-                                                                   TipoMoneda.tmon_Descripcion,
-                                                                
-                                                                    Usuario.usu_Id,
-                                                                    DateTime.Now);
-                    foreach (UDP_RRHH_tbTipoMoneda_Update_Result RES in listTipoMonedas)
-                    {
-                        MensajeError = RES.MensajeError;
-
-                    }
-                    if (!string.IsNullOrEmpty(MensajeError))
-                    {
-                        if (MensajeError.StartsWith("-1"))
-                        {
-                            ModelState.AddModelError("", "1.No se pudo agregar el Registro");
-                            return Json(MensajeError.Substring(0, 2));
-                        }
-                    }
-                    return Json("Exito", JsonRequestBehavior.AllowGet);
-                }
-                catch (Exception ex)
-                {
-                    ex.Message.ToString();
-                    ModelState.AddModelError("", "2.No se pudo agregar el registro");
-                    return Json(MensajeError.Substring(0, 1));
-                }
-
+                db.Entry(tbTipoMonedas).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
             }
-  
-            return Json(TipoMoneda, JsonRequestBehavior.AllowGet);
+            ViewBag.tmon_UsuarioCrea = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTipoMonedas.tmon_UsuarioCrea);
+            ViewBag.tmon_UsuarioModifica = new SelectList(db.tbUsuario, "usu_Id", "usu_NombreUsuario", tbTipoMonedas.tmon_UsuarioModifica);
+            return View(tbTipoMonedas);
         }
 
         // GET: TipoMonedas/Delete/5
