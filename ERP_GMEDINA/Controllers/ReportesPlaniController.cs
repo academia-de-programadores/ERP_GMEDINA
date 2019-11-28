e3bde57e725d8ff5f0cd73b30fb027b636567f34
Carlos Flamenco <57505181+cflamenco99@users.noreply.github.com>
2019-11-28 15:27:05 -0600

    Recovery model

--- ERP_GMEDINA/Controllers/ReportesPlaniController.cs
+++ ERP_GMEDINA/Controllers/ReportesPlaniController.cs
@@ -18,39 +18,7 @@ namespace ERP_GMEDINA.Controllers
         {
             return View();
         }
-
-		ReportesPlaniDS ds = new ReportesPlaniDS();
-		public ActionResult DecimoTercerMesRPT()
-		{
-
-			ReportViewer reportViewer = new ReportViewer();
-			reportViewer.ProcessingMode = ProcessingMode.Local;
-			reportViewer.SizeToReportContent = false;
-			reportViewer.Width = Unit.Pixel(1050);
-			reportViewer.Height = Unit.Pixel(500);
-			reportViewer.BackColor = System.Drawing.Color.White;
-
-			var connectionString = ConfigurationManager.ConnectionStrings["ERP_GMEDINAConnectionString"].ConnectionString;
-			SqlConnection conx = new SqlConnection(connectionString);
-			SqlDataAdapter adp = new SqlDataAdapter("SELECT * FROM rrhh.tbEmpleados", conx);
-
-
-			adp.Fill(ds, ds.UDP_Plani_DecimoTercerMes_RPT.TableName);
-
-			reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"ReportesPlani\DecimoTercerMesRPT.rdlc";
-			reportViewer.LocalReport.DataSources.Add(new ReportDataSource("ReportesPlaniDS", ds.Tables[0]));
-
-
-			ViewBag.ReportViewer = reportViewer;
-
-			return View();
-		}
-
-
-
-
-
-
+		
 
 	}
 }
