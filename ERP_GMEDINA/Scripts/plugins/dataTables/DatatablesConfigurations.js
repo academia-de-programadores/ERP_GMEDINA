cbdd50e3bfbbed0be3b69c2376cb76d98c6e0243
Kevin Aleman <kev4leman@hotmail.com>
2019-12-03 09:15:55 -0600

    datatbles
    cambio al datatables viejo, habiann conflictos

--- ERP_GMEDINA/Scripts/plugins/dataTables/DatatablesConfigurations.js
+++ ERP_GMEDINA/Scripts/plugins/dataTables/DatatablesConfigurations.js
@@ -1,78 +1,29 @@
-﻿var tabla = null;
-var botones = [];
-$(document).ready(function () {
-    var columnas = [];
-    var col = 0;
-    var contador = -1;
-    var head = $("#IndexTable thead tr").find("th").each(function (indice, valor) {
-        contador = contador + 1;
-        campo = valor.innerText;
-        if (campo == "") {
-            columnas.push({
-                className: 'details-control',
-                orderable: false,
-                data: null,
-                defaultContent: ''
-            });
-            col = col + 1;
-        } else if (campo.toUpperCase() == "ID") {
-            columnas.push({
-                data: campo,
-                visible: false
-            });
-            col = col + 1;
-        } else if (campo == "Acciones") {
-            columnas.push({
-                data: null,
-                orderable: false,
-                defaultContent: "<div class='visible-md visible-lg hidden-sm hidden-xs action-buttons'>" +
-                                    "<a class='btn btn-primary btn-xs ' onclick='tablaDetalles(this)' >Detalles</a>" +
-                                    "<a class='btn btn-default btn-xs ' onclick='tablaEditar(this)'>Editar</a>" +
-                                "</div>"
-            });
-        } else {
-            columnas.push({ data: campo });
-            botones.push(contador);
-        }
-    });
-    tabla = $('#IndexTable').DataTable({
+﻿$(document).ready(function () {
+    var Cols = ColCount();
+
+    var Test = $('#IndexTable').DataTable({
         "language": { "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json" },
         responsive: true,
         pageLength: 25,
         dom: '<"html5buttons"B>lTfgitp',
         buttons: [
+            { extend: 'copy' },
+            { extend: 'csv' },
             {
-                extend: 'copy',
+                extend: 'excel', title: 'ExampleFile',
                 exportOptions: {
-                    columns: botones
+                    columns: [Cols]
                 }
             },
             {
-                extend: 'csv',
+                extend: 'pdf', title: 'ExampleFile',
                 exportOptions: {
-                    columns: botones
+                    columns: [Cols]
                 }
-            },
-            {
-                extend: 'excel',
-                exportOptions: {
-                    columns: botones
-                },
-                title: 'ExampleFile'
-            },
-            {
-                extend: 'pdf',
-                exportOptions: {
-                    columns: botones
-                },
-                title: 'nadaaa'
             },
 
             {
                 extend: 'print',
-                exportOptions: {
-                    columns: botones
-                },
                 customize: function (win) {
                     $(win.document.body).addClass('white-bg');
                     $(win.document.body).css('font-size', '10px');
@@ -80,10 +31,22 @@ $(document).ready(function () {
                     $(win.document.body).find('table')
                             .addClass('compact')
                             .css('font-size', 'inherit');
+                },
+                exportOptions: {
+                    columns: [Cols]
                 }
             }
-        ],
-        columns: columnas,
-        order: [[col, 'asc']],
+        ]
     });
 });
+
+function ColCount() {
+    var col = document.getElementById('IndexTable').rows[0].cells.length;
+    console.log(col);
+    var RtrStr = "0";
+
+    for (var i = 1; i < col - 1; i++) {
+        RtrStr += ", " + i.toString();
+    }
+    return RtrStr;
+};
