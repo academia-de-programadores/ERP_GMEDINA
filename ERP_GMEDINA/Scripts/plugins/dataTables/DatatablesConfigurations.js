dee583ac96d18ff1f38641bd86cf888b3a2a018f
Kevin Aleman <kev4leman@hotmail.com>
2019-12-03 11:33:40 -0600

    ghg
    hh

--- ERP_GMEDINA/Scripts/plugins/dataTables/DatatablesConfigurations.js
+++ ERP_GMEDINA/Scripts/plugins/dataTables/DatatablesConfigurations.js
@@ -1,46 +1,63 @@
-﻿$(document).ready(function () {
-    var Cols = ColCount();
-
+﻿var tabla = null;
+var botones = [];
+$(document).ready(function () {
+    var columnas = [];
+    var col = 0;
+    var contador = -1;
+    var head = $("#IndexTable thead tr").find("th").each(function (indice, valor) {
+        contador = contador + 1;
+        campo = valor.innerText;
+        if (campo == "") {
+            columnas.push({
+                className: 'details-control',
+                orderable: false,
+                data: null,
+                defaultContent: ''
+            });
+            col = col + 1;
+        } else if (campo.toUpperCase() == "ID") {
+            columnas.push({
+                data: campo,
+                visible: false
+            });
+            col = col + 1;
+        } else if (campo == "Acciones") {
+            columnas.push({
+                data: null,
+                orderable: false,
+                defaultContent: "<div class='visible-md visible-lg hidden-sm hidden-xs action-buttons'>" +
+                                    "<a class='btn btn-primary btn-xs ' onclick='tablaDetalles(this)' >Detalles</a>" +
+                                    "<a class='btn btn-default btn-xs ' onclick='tablaEditar(this)'>Editar</a>" +
+                                "</div>"
+            });
+        } else {
+            columnas.push({ data: campo });
+        }
+    });
     var Test = $('#IndexTable').DataTable({
         "language": { "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json" },
         responsive: true,
         pageLength: 25,
         dom: '<"html5buttons"B>lTfgitp',
-        buttons: [
-            { extend: 'copy' },
-            { extend: 'csv' },
-            {
-                extend: 'excel', title: 'ExampleFile',
-                exportOptions: {
-                    columns: [Cols]
-                }
-            },
-            {
-                extend: 'pdf', title: 'ExampleFile',
-                exportOptions: {
-                    columns: [Cols]
-                }
-            },
 
-            {
-                extend: 'print',
-                customize: function (win) {
-                    $(win.document.body).addClass('white-bg');
-                    $(win.document.body).css('font-size', '10px');
-
-                    $(win.document.body).find('table')
-                            .addClass('compact')
-                            .css('font-size', 'inherit');
-                },
-                exportOptions: {
-                    columns: [Cols]
-                }
-            }
-        ],
         columns: columnas,
         order: [[col, 'asc']],
     });
 });
+    
+
+
+//$(document).ready(function () {
+//    var Test = $('#IndexTable').DataTable({
+//        "language": { "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json" },
+//        responsive: true,
+//        pageLength: 25,
+//        dom: '<"html5buttons"B>lTfgitp',
+       
+//        columns: columnas,
+//        order: [[col, 'asc']],
+//    });
+//});
 
 function ColCount() {
     var col = document.getElementById('IndexTable').rows[0].cells.length;
