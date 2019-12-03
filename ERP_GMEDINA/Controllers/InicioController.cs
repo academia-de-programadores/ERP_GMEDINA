fc83e89781cedae11e8089ac5376469049ffeff0
EG0782 <53062171+EG0782@users.noreply.github.com>
2019-12-03 09:47:18 -0600

    InicioController 3/12/2019

---
+++ ERP_GMEDINA/Controllers/InicioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERP_GMEDINA.Controllers
+{
+    public class InicioController : Controller
+    {
+        // GET: Inicio
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
