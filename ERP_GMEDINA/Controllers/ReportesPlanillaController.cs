77b1bf3121ad64bff2d7577c756e1055c9bbe9bd
Carlos Flamenco <57505181+cflamenco99@users.noreply.github.com>
2019-12-05 14:38:29 -0600

    jennifer

--- ERP_GMEDINA/Controllers/ReportesPlanillaController.cs
+++ ERP_GMEDINA/Controllers/ReportesPlanillaController.cs
@@ -158,197 +158,190 @@ namespace ERP_GMEDINA.Controllers
 
 			return File(renderedBytes, mimeType);
 		}
-        //Reporte Decimo Cuarto Mes - FIN
-        //-------------------------------------------------------------------------------------------------------------------------------
-        #endregion
+		//Reporte Decimo Cuarto Mes - FIN
+		//-------------------------------------------------------------------------------------------------------------------------------
+		#endregion
 
 
-        #region INFOP
-        //-------------------------------------------------------------------------------------------------------------------------------
-        //Reporte INFOP - INICIO
-
-        //Index 
-        public ActionResult INFOPIndexRPT()
-        {
-            //Cargar DDL del modal (Tipo de planilla a seleccionar)
-
-            ViewBag.TipoPlanillaDDL = new SelectList(db.tbCatalogoDePlanillas, "cpla_IdPlanilla", "cpla_DescripcionPlanilla");
-
-<<<<<<< HEAD
-            ReportViewer reportViewer = new ReportViewer();
-            reportViewer.ProcessingMode = ProcessingMode.Local;
-            reportViewer.SizeToReportContent = true;
-            reportViewer.Width = Unit.Pixel(1050);
-            reportViewer.Height = Unit.Pixel(500);
-            reportViewer.BackColor = System.Drawing.Color.White;
-
-            var connectionString = ConfigurationManager.ConnectionStrings["ERP_GMEDINAConnectionString"].ConnectionString;
+		#region INFOP
+		//-------------------------------------------------------------------------------------------------------------------------------
+		//Reporte INFOP - INICIO
 
 
-            SqlConnection conx = new SqlConnection(connectionString);
-            SqlDataAdapter adp = new SqlDataAdapter("SELECT * FROM Plani.V_INFOP_RPT", conx);
 
-            //adp.Fill(ds, ds.V_INFOP_RPT.TableName);
-
-            reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"ReportesPlanilla\INFOPRPT.rdlc";
-            reportViewer.LocalReport.DataSources.Add(new ReportDataSource("ReportesPlanillaDS", ds.Tables[2]));
+		//Index 
+		public ActionResult INFOPIndexRPT()
+		{
+			//Cargar DDL del modal (Tipo de planilla a seleccionar)
 
 
-            ViewBag.ReportViewerINFOPRPT = reportViewer;
-            return View();
-=======
-            return View(db.V_INFOP_RPT.ToList());
->>>>>>> dbe6f2488ecf99670f7c615f386085e9c0222b12
-        }
 
-        //Reporte con parametros
-        public ActionResult INFOPParametrosRPT(DateTime hipa_FechaPago, int cpla_DescripcionPlanilla, string id)
-        {
-            LocalReport lr = new LocalReport();
-            string path = Path.Combine(Server.MapPath("~/ReportesPlanilla"), "INFOPRPT.rdlc");
-            if (System.IO.File.Exists(path))
-            {
-                lr.ReportPath = path;
-            }
-            else
-            {
-                return View("Index");
-            }
-            List<V_INFOP_RPT> cm = new List<V_INFOP_RPT>();
-
-            cm = db.V_INFOP_RPT.Where(x => hipa_FechaPago == x.hipa_FechaPago && cpla_DescripcionPlanilla == x.cpla_IdPlanilla).ToList();
-
-            ReportDataSource rd = new ReportDataSource("ReportesPlanillaDS", cm);
-            lr.DataSources.Add(rd);
-            string reportType = id;
-            string mimeType;
-            string encoding;
-            string fileNameExtension;
-            string deviceInfo =
-
-            "<DeviceInfo>" +
-            "  <OutputFormat>" + id + "</OutputFormat>" +
-            "  <PageWidth>11in</PageWidth>" +
-            "  <PageHeight>8.5in</PageHeight>" +
-            "  <MarginTop>0.1in</MarginTop>" +
-            "  <MarginLeft>0.1in</MarginLeft>" +
-            "  <MarginRight>0.1in</MarginRight>" +
-            "  <MarginBottom>0.1in</MarginBottom>" +
-            "</DeviceInfo>";
-
-            Warning[] warnings;
-            string[] streams;
-            byte[] renderedBytes;
-
-            renderedBytes = lr.Render(
-                reportType,
-                deviceInfo,
-                out mimeType,
-                out encoding,
-                out fileNameExtension,
-                out streams,
-                out warnings);
-
-            return File(renderedBytes, mimeType);
-        }
-        //Reporte INFOP - FIN
-        //-------------------------------------------------------------------------------------------------------------------------------
-        #endregion
+			ViewBag.TipoPlanillaDDL = new SelectList(db.tbCatalogoDePlanillas, "cpla_IdPlanilla", "cpla_DescripcionPlanilla");
 
 
-        #region Instituciones Financieras
-        //-------------------------------------------------------------------------------------------------------------------------------
-        //Reporte Instituciones Financiras- INICIO
 
-        //Index 
-        public ActionResult InstitucionesFinancierasIndexRPT()
-        { 
-            //Cargar DDL del modal (Tipo de planilla a seleccionar)
-
-            ViewBag.TipoPlanillaDDL = new SelectList(db.tbCatalogoDePlanillas, "cpla_IdPlanilla", "cpla_DescripcionPlanilla");
-<<<<<<< HEAD
-
-            ReportViewer reportViewer = new ReportViewer();
-            reportViewer.ProcessingMode = ProcessingMode.Local;
-            reportViewer.SizeToReportContent = true;
-            reportViewer.Width = Unit.Pixel(1050);
-            reportViewer.Height = Unit.Pixel(500);
-            reportViewer.BackColor = System.Drawing.Color.White;
-            var connectionString = ConfigurationManager.ConnectionStrings["ERP_GMEDINAConnectionString"].ConnectionString;
+			return View(db.V_INFOP_RPT.ToList());
+		}
 
 
-            SqlConnection conx = new SqlConnection(connectionString);
-            SqlDataAdapter adp = new SqlDataAdapter("SELECT * FROM Plani.V_InstitucionesFinancieras_RPT", conx);
 
-            //adp.Fill(ds, ds.V_InstitucionesFinancieras_RPT.TableName);
+		//Reporte con parametros
+		public ActionResult INFOPParametrosRPT(DateTime hipa_FechaPago, int cpla_DescripcionPlanilla, string id)
+		{
+			LocalReport lr = new LocalReport();
+			string path = Path.Combine(Server.MapPath("~/ReportesPlanilla"), "INFOPRPT.rdlc");
+			if (System.IO.File.Exists(path))
+			{
+				lr.ReportPath = path;
+			}
+			else
+			{
+				return View("Index");
+			}
+			List<V_INFOP_RPT> cm = new List<V_INFOP_RPT>();
 
-            reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"ReportesPlanilla\InstitucionesFinancierasRPT.rdlc";
-            reportViewer.LocalReport.DataSources.Add(new ReportDataSource("ReportesPlanillaDS", ds.Tables[3]));
 
 
-            ViewBag.ReportViewerInstitucionesFinancierasRPT = reportViewer;
-            return View();
+			cm = db.V_INFOP_RPT.Where(x => hipa_FechaPago == x.hipa_FechaPago && cpla_DescripcionPlanilla == x.cpla_IdPlanilla).ToList();
 
-=======
-            return View(db.V_InstitucionesFinancieras_RPT.ToList());
->>>>>>> dbe6f2488ecf99670f7c615f386085e9c0222b12
 
-        }
 
-        //Reporte con parametros
-        public ActionResult InstitucionesFinancierasParametrosRPT(DateTime hipa_FechaPago, int cpla_DescripcionPlanilla, string id)
-        {
-            LocalReport lr = new LocalReport();
-            string path = Path.Combine(Server.MapPath("~/ReportesPlanilla"), "InstitucionesFinancierasRPT.rdlc");
-            if (System.IO.File.Exists(path))
-            {
-                lr.ReportPath = path;
-            }
-            else
-            {
-                return View("Index");
-            }
-         
-            List<V_InstitucionesFinancieras_RPT> cm = new List<V_InstitucionesFinancieras_RPT>();
+			ReportDataSource rd = new ReportDataSource("ReportesPlanillaDS", cm);
+			lr.DataSources.Add(rd);
+			string reportType = id;
+			string mimeType;
+			string encoding;
+			string fileNameExtension;
+			string deviceInfo =
 
-            cm = db.V_InstitucionesFinancieras_RPT.Where(x => hipa_FechaPago == x.hipa_FechaPago && cpla_DescripcionPlanilla == x.cpla_IdPlanilla).ToList();
 
-            ReportDataSource rd = new ReportDataSource("ReportesPlanillaDS", cm);
-            lr.DataSources.Add(rd);
-            string reportType = id;
-            string mimeType;
-            string encoding;
-            string fileNameExtension;
-            string deviceInfo =
 
-            "<DeviceInfo>" +
-            "  <OutputFormat>" + id + "</OutputFormat>" +
-            "  <PageWidth>11in</PageWidth>" +
-            "  <PageHeight>8.5in</PageHeight>" +
-            "  <MarginTop>0.1in</MarginTop>" +
-            "  <MarginLeft>0.1in</MarginLeft>" +
-            "  <MarginRight>0.1in</MarginRight>" +
-            "  <MarginBottom>0.1in</MarginBottom>" +
-            "</DeviceInfo>";
+			"<DeviceInfo>" +
+			"  <OutputFormat>" + id + "</OutputFormat>" +
+			"  <PageWidth>11in</PageWidth>" +
+			"  <PageHeight>8.5in</PageHeight>" +
+			"  <MarginTop>0.1in</MarginTop>" +
+			"  <MarginLeft>0.1in</MarginLeft>" +
+			"  <MarginRight>0.1in</MarginRight>" +
+			"  <MarginBottom>0.1in</MarginBottom>" +
+			"</DeviceInfo>";
 
-            Warning[] warnings;
-            string[] streams;
-            byte[] renderedBytes;
 
-            renderedBytes = lr.Render(
-                reportType,
-                deviceInfo,
-                out mimeType,
-                out encoding,
-                out fileNameExtension,
-                out streams,
-                out warnings);
 
-            return File(renderedBytes, mimeType);
-        }
-        //Reporte InstitucionesFinancieras - FIN
-        //-------------------------------------------------------------------------------------------------------------------------------
-        #endregion
-    }
+			Warning[] warnings;
+			string[] streams;
+			byte[] renderedBytes;
+
+
+
+			renderedBytes = lr.Render(
+				reportType,
+				deviceInfo,
+				out mimeType,
+				out encoding,
+				out fileNameExtension,
+				out streams,
+				out warnings);
+
+
+
+			return File(renderedBytes, mimeType);
+		}
+		//Reporte INFOP - FIN
+		//-------------------------------------------------------------------------------------------------------------------------------
+		#endregion
+
+
+		#region Instituciones Financieras
+		//-------------------------------------------------------------------------------------------------------------------------------
+		//Reporte Instituciones Financiras- INICIO
+
+
+
+		//Index 
+		public ActionResult InstitucionesFinancierasIndexRPT()
+		{
+			//Cargar DDL del modal (Tipo de planilla a seleccionar)
+
+
+
+			ViewBag.TipoPlanillaDDL = new SelectList(db.tbCatalogoDePlanillas, "cpla_IdPlanilla", "cpla_DescripcionPlanilla");
+			return View(db.V_InstitucionesFinancieras_RPT.ToList());
+
+
+
+		}
+
+
+
+		//Reporte con parametros
+		public ActionResult InstitucionesFinancierasParametrosRPT(DateTime hipa_FechaPago, int cpla_DescripcionPlanilla, string id)
+		{
+			LocalReport lr = new LocalReport();
+			string path = Path.Combine(Server.MapPath("~/ReportesPlanilla"), "InstitucionesFinancierasRPT.rdlc");
+			if (System.IO.File.Exists(path))
+			{
+				lr.ReportPath = path;
+			}
+			else
+			{
+				return View("Index");
+			}
+
+
+
+			List<V_InstitucionesFinancieras_RPT> cm = new List<V_InstitucionesFinancieras_RPT>();
+
+
+
+			cm = db.V_InstitucionesFinancieras_RPT.Where(x => hipa_FechaPago == x.hipa_FechaPago && cpla_DescripcionPlanilla == x.cpla_IdPlanilla).ToList();
+
+
+
+			ReportDataSource rd = new ReportDataSource("ReportesPlanillaDS", cm);
+			lr.DataSources.Add(rd);
+			string reportType = id;
+			string mimeType;
+			string encoding;
+			string fileNameExtension;
+			string deviceInfo =
+
+
+
+			"<DeviceInfo>" +
+			"  <OutputFormat>" + id + "</OutputFormat>" +
+			"  <PageWidth>11in</PageWidth>" +
+			"  <PageHeight>8.5in</PageHeight>" +
+			"  <MarginTop>0.1in</MarginTop>" +
+			"  <MarginLeft>0.1in</MarginLeft>" +
+			"  <MarginRight>0.1in</MarginRight>" +
+			"  <MarginBottom>0.1in</MarginBottom>" +
+			"</DeviceInfo>";
+
+
+
+			Warning[] warnings;
+			string[] streams;
+			byte[] renderedBytes;
+
+
+
+			renderedBytes = lr.Render(
+				reportType,
+				deviceInfo,
+				out mimeType,
+				out encoding,
+				out fileNameExtension,
+				out streams,
+				out warnings);
+
+
+
+			return File(renderedBytes, mimeType);
+		}
+		//Reporte InstitucionesFinancieras - FIN
+		//-------------------------------------------------------------------------------------------------------------------------------
+		#endregion
+	}
 
 }
