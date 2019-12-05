3c14fcba98be58d49345bd177b925dced51f57b0
Walter <walter.ortega@bi-dss.com>
2019-12-05 16:22:20 -0600

    commit walter/comienzo reportes

--- ERP_GMEDINA/Controllers/ReportesPlanillaController.cs
+++ ERP_GMEDINA/Controllers/ReportesPlanillaController.cs
@@ -379,5 +379,101 @@ namespace ERP_GMEDINA.Controllers
         }
         //Reporte InstitucionesFinancieras - FIN
         //-------------------------------------------------------------------------------------------------------------------------------
+        #region Reporte IHSS
+        //-------------------------------------------------------------------------------------------------------------------------------
+        //Reporte Decimo Cuarto Mes - INICIO
+
+        //Index 
+        public ActionResult IHSSIndexRPT()
+        {
+            //Cargar DDL del modal (Tipo de planilla a seleccionar)
+
+            ViewBag.TipoPlanillaDDL = new SelectList(db.tbCatalogoDePlanillas, "cpla_IdPlanilla", "cpla_DescripcionPlanilla");
+
+            ReportViewer reportViewer = new ReportViewer();
+            reportViewer.ProcessingMode = ProcessingMode.Local;
+            reportViewer.SizeToReportContent = true;
+            reportViewer.Width = Unit.Pixel(1050);
+            reportViewer.Height = Unit.Pixel(450);
+            reportViewer.BackColor = System.Drawing.Color.White;
+
+            var connectionString = ConfigurationManager.ConnectionStrings["ERP_GMEDINAConnectionString"].ConnectionString;
+
+
+            SqlConnection conx = new SqlConnection(connectionString);
+            SqlDataAdapter adp = new SqlDataAdapter("SELECT * FROM Plani.V_IHSS_RPT", conx);
+
+            adp.Fill(ds, ds.V_IHSS_RPT.TableName);
+
+            reportViewer.LocalReport.ReportPath = Request.MapPath(Request.ApplicationPath) + @"ReportesPlanilla\IHSSRPT.rdlc";
+            reportViewer.LocalReport.DataSources.Add(new ReportDataSource("ReportesPlanillaDS", ds.Tables[1]));
+
+
+            ViewBag.ReportViewerIHSSRPT = reportViewer;
+            return View();
+        }
+
+        //Reporte con parametros
+        public ActionResult IHSSRPT(DateTime hipa_FechaPago, int cpla_DescripcionPlanilla, string id)
+        {
+            LocalReport lr = new LocalReport();
+            string path = Path.Combine(Server.MapPath("~/ReportesPlanilla"), "IHSSRPT.rdlc");
+            if (System.IO.File.Exists(path))
+            {
+                lr.ReportPath = path;
+            }
+            else
+            {
+                return View("Index");
+            }
+            List<V_IHSS_RPT> cm = new List<V_IHSS_RPT>();
+
+            cm = db.V_IHSS_RPT.Where(x => hipa_FechaPago == x.hipa_FechaPago && cpla_DescripcionPlanilla == x.cpla_IdPlanilla).ToList();
+
+            ReportDataSource rd = new ReportDataSource("ReportesPlanillaDS", cm);
+            lr.DataSources.Add(rd);
+            string reportType = id;
+            string mimeType;
+            string encoding;
+            string fileNameExtension;
+            string deviceInfo =
+
+            "<DeviceInfo>" +
+            "  <OutputFormat>" + id + "</OutputFormat>" +
+            "  <PageWidth>11in</PageWidth>" +
+            "  <PageHeight>8.5in</PageHeight>" +
+            "  <MarginTop>0.1in</MarginTop>" +
+            "  <MarginLeft>0.1in</MarginLeft>" +
+            "  <MarginRight>0.1in</MarginRight>" +
+            "  <MarginBottom>0.1in</MarginBottom>" +
+            "</DeviceInfo>";
+
+            Warning[] warnings;
+            string[] streams;
+            byte[] renderedBytes;
+
+            renderedBytes = lr.Render(
+                reportType,
+                deviceInfo,
+                out mimeType,
+                out encoding,
+                out fileNameExtension,
+                out streams,
+                out warnings);
+
+            return File(renderedBytes, mimeType);
+        }
+        //Reporte Decimo Cuarto Mes - FIN
+        //-------------------------------------------------------------------------------------------------------------------------------
+        #endregion
+
+        #region Reporte Impuesto sobre la Renta(ISR)
+        #endregion
+
+        #region Reporte Liquidaciones
+        #endregion
+
+
+
     }
 }
