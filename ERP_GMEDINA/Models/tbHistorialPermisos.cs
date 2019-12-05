bf259bdcd72d4b924cd056df4e3e42dc9bcfdb04
Kevin Alemán <57551666+Kevin4leman@users.noreply.github.com>
2019-12-05 15:16:48 -0600

    Revert "prueba"
    This reverts commit 254e15ebab896844dc41c6b55912d4bffcabb6b1.

--- ERP_GMEDINA/Models/tbHistorialPermisos.cs
+++ ERP_GMEDINA/Models/tbHistorialPermisos.cs
@@ -21,7 +21,6 @@ namespace ERP_GMEDINA.Models
         public System.DateTime hper_fechaFin { get; set; }
         public int hper_Duracion { get; set; }
         public string hper_Observacion { get; set; }
-        public int hper_PorcentajeIndemnizado { get; set; }
         public bool hper_Estado { get; set; }
         public string hper_RazonInactivo { get; set; }
         public int hper_UsuarioCrea { get; set; }
@@ -29,9 +28,8 @@ namespace ERP_GMEDINA.Models
         public Nullable<int> hper_UsuarioModifica { get; set; }
         public Nullable<System.DateTime> hper_FechaModifica { get; set; }
     
-        public virtual tbEmpleados tbEmpleados { get; set; }
-        public virtual tbTipoPermisos tbTipoPermisos { get; set; }
         public virtual tbUsuario tbUsuario { get; set; }
         public virtual tbUsuario tbUsuario1 { get; set; }
+        public virtual tbEmpleados tbEmpleados { get; set; }
     }
 }
