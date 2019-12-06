baaa68f00e426ff23bd479ad51c9374b200b8ae4
Carlos Flamenco <57505181+cflamenco99@users.noreply.github.com>
2019-12-06 13:29:35 -0600

    REPORTES Y MENU

---
+++ ERP_GMEDINA/Models/cGeneralTotalesRPT.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ERP_GMEDINA.Models
+{
+
+    [MetadataType(typeof(cGeneralTotalesRPT))]
+    public partial class V_GeneralTotales_RPT
+    {
+
+    }
+
+    public class cGeneralTotalesRPT
+    {
+
+        [Display(Name = "Fecha Pago")]
+        [Required(ErrorMessage = "No puede dejar campos vacios.")]
+        public System.DateTime hipa_FechaPago { get; set; }
+
+        [Display(Name = "Codigo Planilla")]
+        public int cpla_IdPlanilla { get; set; }
+
+        [Display(Name = "Tipo Planilla")]
+        [Required(ErrorMessage = "No puede dejar campos vacios.")]
+        public string cpla_DescripcionPlanilla { get; set; }
+
+        [Display(Name = "Total ISR")]
+        public Nullable<decimal> cde_TotalISR { get; set; }
+
+        [Display(Name = "Total AFP")]
+        public Nullable<decimal> cde_TotalAFP { get; set; }
+
+        [Display(Name = "Total IHSS")]
+        public Nullable<decimal> cde_TotalIHSS { get; set; }
+
+        [Display(Name = "Total RAP")]
+        public Nullable<decimal> cde_TotalRAP { get; set; }
+
+        [Display(Name = "Total INFOP")]
+        public Nullable<decimal> cde_TotalINFOP { get; set; }
+
+        [Display(Name = "Otras Deducciones")]
+        public Nullable<decimal> cde_OtrasDeducciones { get; set; }
+
+    }
+}
--- ERP_GMEDINA/Scripts/plugins/dataTables/DatatablesConfigurations.js
+++ ERP_GMEDINA/Scripts/plugins/dataTables/DatatablesConfigurations.js
@@ -2,7 +2,7 @@
 	$('.dataTables-example').DataTable({
 		"language": { "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Spanish.json" },
 		responsive: true,
-		pageLength: 25,
+		pageLength: 10,
 		dom: '<"html5buttons"B>lTfgitp',
 		buttons: [
             {
@@ -25,39 +25,6 @@
             	},
             	className: 'btn btn-primary',
             	title: 'Excel'
-            },
-            {
-            	extend: 'pdf',
-            	text: '<i class="fa fa-file-pdf-o btn-xs"></i>',
-            	titleAttr: 'PDF',
-            	exportOptions: {
-            		columns: [0, 1, 2, 3, 4],
-            	},
-            	className: 'btn btn-primary',
-            	title: 'PDF'
-            },
-            {
-            	extend: 'print',
-            	customize: function (win) {
-            		$(win.document.body)
-                        .css('font-size', '10pt')
-                        .prepend($('<img />')
-                            .attr('src', 'Hospital_BI/assets/img/bi-stroke.png')
-                            .addClass('asset-print-img')
-                        );
-
-            		$(win.document.body).find('table')
-                        .addClass('compact')
-                        .css('font-size', 'inherit');
-            	},
-            	messageTop: 'Reporte',
-
-            	exportOptions: {
-            		columns: [0, 1, 2, 3, 4],
-            	},
-            	className: 'btn btn-primary',
-            	text: '<i class="fa fa-print btn-xs"></i>',
-            	titleAttr: 'Imprimir'
             }
 
 		]
