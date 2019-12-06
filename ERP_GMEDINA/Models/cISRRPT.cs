5b5b66dd63b6c1bfec5e82a2f9a5875fb221a483
Carlos Flamenco <57505181+cflamenco99@users.noreply.github.com>
2019-12-06 14:39:39 -0600

    2 REPORTES

---
+++ ERP_GMEDINA/Models/cISRRPT.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ERP_GMEDINA.Models
+{
+
+    [MetadataType(typeof(cISRRPT))]
+    public partial class V_ISR_RPT
+    {
+
+    }
+    public class cISRRPT
+    {
+        [Display(Name = "Codigo Empleado")]
+        public int emp_Id { get; set; }
+
+        public string per_Identidad { get; set; }
+
+        [Display(Name = "Nombre Completo")]
+        public string NombreCompleto { get; set; }
+
+        public int depto_Id { get; set; }
+
+        [Display(Name = "Departamento")]
+        public string depto_descripcion { get; set; }
+
+        public int area_Id { get; set; }
+
+        [Display(Name = "Area")]
+        public string area_Descripcion { get; set; }
+
+        public int cpla_IdPlanilla { get; set; }
+
+        [Display(Name = "Planilla")]
+        [Required(ErrorMessage = "No puede dejar campos vacios.")]
+        public string cpla_DescripcionPlanilla { get; set; }
+
+        [Display(Name = "Total ISR")]
+        public decimal hipa_TotalISR { get; set; }
+
+        [Display(Name = "Sueldo Neto")]
+        public Nullable<decimal> hipa_SueldoNeto { get; set; }
+
+        [Display(Name = "Fecha Pago")]
+        [Required(ErrorMessage = "No puede dejar campos vacios.")]
+        public System.DateTime hipa_FechaPago { get; set; }
+    }
+}
---
+++ ERP_GMEDINA/Scripts/app/General/ISRRPT.js
@@ -0,0 +1,60 @@
+﻿//FUNCION GENERICA PARA REUTILIZAR AJAX
+function _ajax(params, uri, type, callback) {
+    $.ajax({
+        url: uri,
+        type: type,
+        data: { params },
+        success: function (data) {
+            callback(data);
+        }
+    });
+}
+
+$(document).ready(function () {
+    console.clear();
+});
+
+
+// MOSTRAR MODAL DE FECHAS
+$(document).on("click", "#ParametrosISRRPT", function () {
+    $("#ISRModalRPT").modal();
+});
+
+// OCULTAR MODAL DE FECHAS
+$("#btnCerrarModalRPT").click(function () {
+    $("#ISRModalRPT").modal('hide');
+});
+
+
+//FUNCION: OCULTAR DATA ANNOTATION CON BOTON INFERIOR CERRAR DEL MODAL.
+$("#btnCerrarModalRPT").click(function () {
+    $("#Validation_descipcion").css("display", "none");
+    $("#Validation_descipcion2").css("display", "none");
+
+    $("#hipa_FechaPago").val('');
+    $("#cpla_DescripcionPlanilla").val('');
+
+});
+
+
+//FUNCION: OCULTAR DATA ANNOTATION CON BOTON SUPERIOR DE CERRAR (BOTON CON X).
+$("#IconCerrar").click(function () {
+    $("#Validation_descipcion").css("display", "none");
+    $("#Validation_descipcion2").css("display", "none");
+
+    $("#hipa_FechaPago").val('');
+    $("#cpla_DescripcionPlanilla").val('');
+});
+
+
+//FUNCION: MOSTRAR DATA ANNOTATION SI LOS CAMPOS SIGUEN VACIOS (EN CASO DE USO CONTINUO PREVIO AL CIERRE DEL MODAL).
+$("#btnEnviarParametros").click(function () {
+    var Parametro1 = $("#hipa_FechaPago").val();
+    var Parametro2 = $("#cpla_DescripcionPlanilla").val();
+
+    if (Parametro1 == "" || Parametro2 == "") {
+        $("#Validation_descipcion").css("display", "");
+        $("#Validation_descipcion2").css("display", "");
+    }
+
+});
