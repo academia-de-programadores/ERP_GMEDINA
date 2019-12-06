67c4ed295c6fe3fdfff191852ae8a6e24bb81e8c
Chris Andino <chrisag40@gmail.com>
2019-12-05 21:21:02 -0600

    Botones de loading

--- ERP_GMEDINA/Scripts/app/General/Jornadas/IndexJornadas.js
+++ ERP_GMEDINA/Scripts/app/General/Jornadas/IndexJornadas.js
@@ -57,12 +57,14 @@ $('#IndexTable tbody').on('click', 'td.details-control', function () {
     else {
         id = row.data().ID;
         hola = row.data().hola;
+        tr.addClass('loading');
         _ajax({ id: parseInt(id) },
             '/Jornadas/ChildRowData',
             'GET',
             function (obj) {
                 if (obj != "-1" && obj != "-2" && obj != "-3") {
                     row.child(format(obj)).show();
+                    tr.removeClass('loading');
                     tr.addClass('shown');
                 }
             });
