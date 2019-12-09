3cc70e914524536fe92d1599f16942268d0920f1
AllanBennett27 <53162492+AllanBennett27@users.noreply.github.com>
2019-12-09 09:10:02 -0600

    Modificado 09/12/2019 9:10 a.m
    *Se dibuja la tabla completa
    *Se agregaron los botones de accion
    *Y el boton de agregar

--- ERP_GMEDINA/Scripts/app/General/HistorialAmonestaciones/IndexHistorialAmonestaciones.js
+++ ERP_GMEDINA/Scripts/app/General/HistorialAmonestaciones/IndexHistorialAmonestaciones.js
@@ -1,10 +1,11 @@
 ﻿function format(obj) {
-    var div = '<div class="ibox"><div class="ibox-title"><h5>Amonestaciones</h5></div><div class="ibox-content"><div class="row">' + '<table class="table table-striped table-borderef table-hover dataTables-example"> ' +
+    var div = '<div class="ibox"><div class="ibox-title"><h5>Amonestaciones</h5><div align=right> <button type="button" class="btn btn-primary btn-xs" id="btnEditarIngreso" data-id="@item.hamo_Id">Agregar Amonestación</button></div></div><div class="ibox-content"><div class="row">' + '<table class="table table-striped table-borderef table-hover dataTables-example"> ' +
         '<thead>' +
             '<tr>' +
-                '<th>' + 'Tipo Amostacion' + '</th>' +
-                '<th>' + 'Fecha Amonestacion' + '</th>' +
-                '<th>' + 'Obsevarcion Amonestacion' + '</th>' +
+                '<th>' + 'Tipo Amonestacion' + '</th>' +
+                '<th>' + 'Fecha' + '</th>' +
+                '<th>' + 'Obsevarcion' + '</th>' +
+                '<th>' + 'Acciones' + '</th>' +
                 '</tr>' +
                 '</thead>';
     obj.forEach(function (index, value) {
@@ -12,8 +13,9 @@
                 '<tbody>' +
                 '<tr>' +
                 '<td>'+ index.tamo_Descripcion + '</td>'+
-                '<td>'+ index.hamo_Fecha + '</td>'+
-                '<td>'+ index.hamo_Observacion + '</td>'+
+                '<td>' + index.hamo_Fecha + '</td>' +
+                '<td>' + index.hamo_Observacion + '</td>' +
+                '<td>'+' <button type="button" class="btn btn-primary btn-xs" id="btnEditarIngreso" data-id="@item.hamo_Id">Editar</button> <button type="button" class="btn btn-default btn-xs" id="btnDetalle" data-id="@item.hamo_Id">Detalle</button>'+'</td>'+
                 '</tr>' +
                 '</tbody>' 
                 '</table>'
