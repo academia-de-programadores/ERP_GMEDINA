16aa6cba2f11fd512f5ef726bc83de4f3eb60834
AllanBennett27 <53162492+AllanBennett27@users.noreply.github.com>
2019-12-12 15:20:32 -0600

    Modificado 12/12/2019 03:20 a.m

---
+++ ERP_GMEDINA/Models/cTipoAmonestaciones.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ERP_GMEDINA.Models
+{
+    [MetadataType(typeof(cTipoAmonestaciones))]
+    public partial class tbTipoAmonestaciones
+    {
+
+    }
+    public class cTipoAmonestaciones
+    {
+        [Display(Name = "Tipo Amonestacion")]
+        public int tamo_Id { get; set; }
+        //[Display(Name = "Tipo Amonestacion")]
+        public string tamo_Descripcion { get; set; }
+    }
+}
