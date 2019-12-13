cd026d837dbeee1664869a77e1148486f636fd27
keyci1113 <47374214+keyci1113@users.noreply.github.com>
2019-12-13 10:05:39 -0600

    VF

--- ERP_GMEDINA/Controllers/EmpleadosController.cs
+++ ERP_GMEDINA/Controllers/EmpleadosController.cs
@@ -80,24 +80,40 @@ namespace ERP_GMEDINA.Controllers
         public void ArchivoEmpleados()
         {
             List<ExcelEmpleados> ExcelEmpleados = new List<ExcelEmpleados>();
-            ExcelEmpleados.Add(new ExcelEmpleados() { per_Identidad = "", per_Nombres = "5", per_Apellidos = "Jan", per_FechaNacimiento = "2019", per_Sexo = "", nac_Id = "", per_Direccion = "", per_Telefono = "", per_CorreoElectronico = "", per_EstadoCivil = "", per_TipoSangre = "", Cargo = db.UDP_RRHH_tbCargos_tbEmpleados_Select().ToList(), area_Id = "", depto_Id = "", jor_Id = "" });
+            ExcelEmpleados.Add(new ExcelEmpleados() { per_Identidad = "", per_Nombres = "", per_Apellidos = "", per_FechaNacimiento = "", per_Sexo = "", nac_Id = "", per_Direccion = "", per_Telefono = "", per_CorreoElectronico = "", per_EstadoCivil = "", per_TipoSangre = "", Cargo = db.UDP_RRHH_tbCargos_tbEmpleados_Select().ToList(), area_Id = "", depto_Id = "", jor_Id = "" });
             ExcelPackage Ep = new ExcelPackage();
             ExcelWorksheet Sheet = Ep.Workbook.Worksheets.Add("ArchivoEmpleados");
-            Sheet.Cells["A1"].Value = "Identidad";
-            Sheet.Cells["B1"].Value = "Nombres";
-            Sheet.Cells["C1"].Value = "Apellidos";
-            Sheet.Cells["D1"].Value = "Fecha Nacimiento";
-            Sheet.Cells["E1"].Value = "Sexo";
-            Sheet.Cells["F1"].Value = "Nacionalidad";
-            Sheet.Cells["G1"].Value = "Direccion";
-            Sheet.Cells["H1"].Value = "Telefono";
-            Sheet.Cells["I1"].Value = "Correo Electronico";
-            Sheet.Cells["J1"].Value = "Estado Civil";
-            Sheet.Cells["K1"].Value = "Tipo de Sangre";
-            Sheet.Cells["L1"].Value = "Cargo";
-            Sheet.Cells["M1"].Value = "Area";
-            Sheet.Cells["N1"].Value = "Departamentos";
-            Sheet.Cells["O1"].Value = "Jornadas";
+            Sheet.Cells["A1:O3"].Merge = true;
+            Sheet.Cells["A1:O3"].Style.Font.Size = 16;
+            Sheet.Cells["A1:O3"].Style.Font.Color.SetColor(System.Drawing.Color.Red);
+            Sheet.Cells["A1:O3"].Value = "FAVOR LLENAR UNICAMENTE LA INFORMACION SOLICITADA, NO CAMBIAR NINGUNA CONFIGURACION DE ESTE DOCUMENTO";
+            Sheet.Cells["A1:O3"].Style.VerticalAlignment = OfficeOpenXml.Style.ExcelVerticalAlignment.Center;
+            Sheet.Cells["A1:O3"].Style.HorizontalAlignment = OfficeOpenXml.Style.ExcelHorizontalAlignment.Center;
+            Sheet.Cells["A1:O3"].Style.Border.Top.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+            Sheet.Cells["A1:O3"].Style.Border.Left.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+            Sheet.Cells["A1:O3"].Style.Border.Right.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+            Sheet.Cells["A1:O3"].Style.Border.Bottom.Style = OfficeOpenXml.Style.ExcelBorderStyle.Thin;
+            Sheet.Cells["A1:O3"].Style.Border.Top.Color.SetColor(System.Drawing.Color.Red);
+            Sheet.Cells["A1:O3"].Style.Border.Left.Color.SetColor(System.Drawing.Color.Red);
+            Sheet.Cells["A1:O3"].Style.Border.Right.Color.SetColor(System.Drawing.Color.Red);
+            Sheet.Cells["A1:O3"].Style.Border.Bottom.Color.SetColor(System.Drawing.Color.Red);
+            Sheet.Cells["A1:O3"].Style.Fill.PatternType = OfficeOpenXml.Style.ExcelFillStyle.Solid;
+            Sheet.Cells["A1:O3"].Style.Fill.BackgroundColor.SetColor(System.Drawing.ColorTranslator.FromHtml("#f0f3f5"));
+            Sheet.Cells["A4"].Value = "Identidad";
+            Sheet.Cells["B4"].Value = "Nombres";
+            Sheet.Cells["C4"].Value = "Apellidos";
+            Sheet.Cells["D4"].Value = "Fecha Nacimiento";
+            Sheet.Cells["E4"].Value = "Sexo";
+            Sheet.Cells["F4"].Value = "Nacionalidad";
+            Sheet.Cells["G4"].Value = "Direccion";
+            Sheet.Cells["H4"].Value = "Telefono";
+            Sheet.Cells["I4"].Value = "Correo Electronico";
+            Sheet.Cells["J4"].Value = "Estado Civil";
+            Sheet.Cells["K4"].Value = "Tipo de Sangre";
+            Sheet.Cells["L4"].Value = "Cargo";
+            Sheet.Cells["M4"].Value = "Area";
+            Sheet.Cells["N4"].Value = "Departamentos";
+            Sheet.Cells["O4"].Value = "Jornadas";
 
 
 
@@ -108,39 +124,49 @@ namespace ERP_GMEDINA.Controllers
                 Sheet.Cells[string.Format("B{0}", row)].Value = item.per_Nombres;
                 Sheet.Cells[string.Format("C{0}", row)].Value = item.per_Apellidos;
                 Sheet.Cells[string.Format("D{0}", row)].Value = item.per_FechaNacimiento;
-                Sheet.Cells[string.Format("E{0}", row)].Value = item.per_Sexo;
+                Sheet.Cells["D5"].Style.Numberformat.Format = "yyyy-mm-dd";
+                //Sheet.Cells["D5"].Formula = "=DATE(2014,10,5)";
+
+                var per_Sexo = Sheet.DataValidations.AddListValidation("E5");
+                per_Sexo.Formula.Values.Add("F");
+                per_Sexo.Formula.Values.Add("M");
+                //Sheet.Cells[string.Format("E{0}", row)].Value = item.per_Sexo;
 
                 var Nacionalidades = db.tbNacionalidades
                     .Select(tabla => tabla.nac_Descripcion)
                     .ToArray();
                 Sheet.Cells["MB1"].LoadFromCollection<string>(Nacionalidades.ToList<string>());
-                var Nac_val = Sheet.DataValidations.AddListValidation("F2");
+                var Nac_val = Sheet.DataValidations.AddListValidation("F5");
                 Nac_val.Formula.ExcelFormula = "$MB$1:$MB$" + Nacionalidades.Length;
                 Sheet.Column(340).Style.Font.Color.SetColor(System.Drawing.Color.White);
 
                 Sheet.Cells[string.Format("G{0}", row)].Value = item.per_Direccion;
                 Sheet.Cells[string.Format("H{0}", row)].Value = item.per_Telefono;
                 Sheet.Cells[string.Format("I{0}", row)].Value = item.per_CorreoElectronico;
-                Sheet.Cells[string.Format("J{0}", row)].Value = item.per_EstadoCivil;
+
+                var per_EstadoCivil = Sheet.DataValidations.AddListValidation("J5");
+                per_EstadoCivil.Formula.Values.Add("S");
+                per_EstadoCivil.Formula.Values.Add("C");
+                per_EstadoCivil.Formula.Values.Add("D");
+                per_EstadoCivil.Formula.Values.Add("V");
+                //Sheet.Cells[string.Format("J{0}", row)].Value = item.per_EstadoCivil;
                 Sheet.Cells[string.Format("K{0}", row)].Value = item.per_TipoSangre;
 
                 var Cargos = db.tbCargos
                     .Select(tabla=>tabla.car_Descripcion)
                     .ToArray();
                 Sheet.Cells["MA1"].LoadFromCollection<string>(Cargos.ToList<string>());
-                var Car_val = Sheet.DataValidations.AddListValidation("L2");
+                var Car_val = Sheet.DataValidations.AddListValidation("L5");
                 Car_val.Formula.ExcelFormula = "$MA$1:$MA$"+ Cargos.Length;
                 Sheet.Column(339).Style.Font.Color.SetColor(System.Drawing.Color.White);
-                //Sheet.Column(15).Hidden = true;
-                //Sheet.Cells.AutoFitColumns(8.43, 100);
-
+            
                 //agregar parte de :"FAVOR LLENAR UNICAMENTE LA INFORMACION SOLICITADA, NO CAMBIAR NINGUNA CONFIGURACION DE ESTE DOCUMENTO"
 
                 var Areas = db.tbAreas
                 .Select(tabla => tabla.area_Descripcion)
                 .ToArray();
                 Sheet.Cells["MC1"].LoadFromCollection<string>(Areas.ToList<string>());
-                var area_val = Sheet.DataValidations.AddListValidation("M2");
+                var area_val = Sheet.DataValidations.AddListValidation("M5");
                 area_val.Formula.ExcelFormula = "$MC$1:$MC$" + Areas.Length;
                 Sheet.Column(341).Style.Font.Color.SetColor(System.Drawing.Color.White);
 
@@ -149,7 +175,7 @@ namespace ERP_GMEDINA.Controllers
                 .Select(tabla => tabla.depto_Descripcion)
                 .ToArray();
                 Sheet.Cells["MD1"].LoadFromCollection<string>(Dpto.ToList<string>());
-                var Dpto_val = Sheet.DataValidations.AddListValidation("N2");
+                var Dpto_val = Sheet.DataValidations.AddListValidation("N5");
                 Dpto_val.Formula.ExcelFormula = "$MD$1:$MD$" + Dpto.Length;
                 Sheet.Column(342).Style.Font.Color.SetColor(System.Drawing.Color.White);
 
@@ -157,7 +183,7 @@ namespace ERP_GMEDINA.Controllers
                .Select(tabla => tabla.jor_Descripcion)
                .ToArray();
                 Sheet.Cells["ME1"].LoadFromCollection<string>(Jor.ToList<string>());
-                var jor_val = Sheet.DataValidations.AddListValidation("O2");
+                var jor_val = Sheet.DataValidations.AddListValidation("O5");
                 jor_val.Formula.ExcelFormula = "$ME$1:$ME$" + Jor.Length;
                 Sheet.Column(342).Style.Font.Color.SetColor(System.Drawing.Color.White);
 
