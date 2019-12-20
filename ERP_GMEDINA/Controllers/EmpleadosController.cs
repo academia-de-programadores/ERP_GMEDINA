f791aa3fc468731c8b2c60c921fa96c5df36cf3f
keyci1113 <47374214+keyci1113@users.noreply.github.com>
2019-12-20 16:35:11 -0600

    inicio de lectura del archivo

--- ERP_GMEDINA/Controllers/EmpleadosController.cs
+++ ERP_GMEDINA/Controllers/EmpleadosController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
+using System.IO;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
@@ -197,11 +198,71 @@ namespace ERP_GMEDINA.Controllers
             Response.End();
         }
 
+        [HttpPost]
+        public ActionResult UploadEmpleados(string FilePath) {
+            ////foreach (string upload in Request.Files)
+            ////{
+            ////    if (Request.Files[upload].FileName != "")
+            ////    {
+            ////        string path = AppDomain.CurrentDomain.BaseDirectory + "/App_Data/uploads/";
+            ////        string filename = Path.GetFileName(Request.Files[upload].FileName);
+            ////        Request.Files[upload].SaveAs(Path.Combine(path, filename));
+            ////    }
+            ////}
+            ////return View("Upload");
+            //string filePath = string.Empty;
+            //if (postedFile != null)
+            //{
+            //    string path = Server.MapPath("~/Uploads/");
+            //    if (!Directory.Exists(path))
+            //    {
+            //        Directory.CreateDirectory(path);
+            //    }
 
-        public void ArchveEmpleados() {
-
-
+            //}
+            FileInfo existingFile = new FileInfo(FilePath);
+            using (ExcelPackage package = new ExcelPackage(existingFile))
+            {
+                //get the first worksheet in the workbook
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[1];
+                int colCount = worksheet.Dimension.End.Column;  //get Column Count
+                int rowCount = worksheet.Dimension.End.Row;     //get row count
+                string queryString = "INSERT INTO tableName VALUES";        //Here I am using "blind insert". You can specify the column names Blient inset is strongly not recommanded
+                string eachVal = "";
+            }
+            //    bool status;
+            //    for (int row = 1; row <= rowCount; row++)
+            //    {
+            //        queryString += "(";
+            //        for (int col = 1; col <= colCount; col++)
+            //        {
+            //            eachVal = worksheet.Cells[row, col].Value.ToString().Trim();
+            //            queryString += "'" + eachVal + "',";
+            //        }
+            //        queryString = queryString.Remove(queryString.Length - 1, 1);    //removing last comma (,) from the string
+            //        if (row % 1000 == 0)        //On every 1000 query will execute, as maximum of 1000 will be executed at a time. 
+            //        {
+            //            queryString += ")";
+            //            status = this.runQuery(queryString);    //executing query
+            //            if (status == false)
+            //                return status;
+            //            queryString = "INSERT INTO tableName VALUES";
+            //        }
+            //        else
+            //        {
+            //            queryString += "),";
+            //        }
+            //    }
+            //    queryString = queryString.Remove(queryString.Length - 1, 1);    //removing last comma (,) from the string
+            //    status = this.runQuery(queryString);    //executing query
+            //    return status;
+            //}
+            //return status;
+            return View("Upload");
         }
+
+
+
 
         // GET: Empleados/Details/5
         public ActionResult Details(int? id)
--- ERP_GMEDINA/Scripts/app/General/Empleados/AgregarEmpleado.js
+++ ERP_GMEDINA/Scripts/app/General/Empleados/AgregarEmpleado.js
@@ -3,3 +3,30 @@
     modalnuevo.modal('show');
     
 });
+$("#btnUpload").change(function () {
+    var fileExtension = ['xls','xlsx'];
+    if ($.inArray($(this).val().split('.').pop().toLowerCase(), fileExtension) == -1) {
+        MsgError("¡Error!", "Debe Agregar el archivo de excel correspondiente");
+    }
+});
+$("#btnGuardar").click(function () {
+    var data = new FormData();
+    var Archivo="ArchivoEmpleados";
+    var files = $("#btnUpload").get(0).files;
+    if (files.length == 0) {
+        MsgError("¡Error!", "Debe Agregar el archivo de excel");
+
+    } else {
+        data.append("UploadedFile", files[0]);
+
+    }
+    var ajaxRequest = $.ajax({
+        type: "POST",
+        url: '/Empleados/UploadEmpleados',
+        contentType: false,
+        processData: false,
+        data: data
+    });
+
+          
+});
