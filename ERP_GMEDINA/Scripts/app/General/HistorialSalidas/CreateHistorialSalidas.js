8ce4a54c70809361c19a7aa7cd01bfeb6f8d8836
AxelGuzman <53068443+AxelGuzman@users.noreply.github.com>
2019-12-22 00:10:20 -0600

    filtro fecha funcional

--- ERP_GMEDINA/Scripts/app/General/HistorialSalidas/CreateHistorialSalidas.js
+++ ERP_GMEDINA/Scripts/app/General/HistorialSalidas/CreateHistorialSalidas.js
@@ -133,9 +133,25 @@ function Remover(btn) {
 }
 //Llamamos los dropdowns
 $(document).ready(function () {
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd
+    }
+    if (mm < 10) {
+        mm = '0' + mm
+    }
+
+    today = yyyy + '-' + mm + '-' + dd;
+    $("#hsal_FechaSalida").attr("max", today);
+
     llenarDropDowlistEmpleados();
     llenarDropDowlistTipoSalida();
-    llenarDropDowlistRazonSalida();
+    llenarDropDowlistRazonSalida(); 
+
     ChildTable = $(ChildDataTable).DataTable({
         pageLength: 3,
         lengthChange: false,
