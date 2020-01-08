3184d6b643eb5f65c2f4f7a45e52a798f1bc554f
keyci1113 <47374214+keyci1113@users.noreply.github.com>
2020-01-08 09:02:10 -0600

    funcionalidad del menu

--- ERP_GMEDINA/Scripts/app/Menu/Menu.js
+++ ERP_GMEDINA/Scripts/app/Menu/Menu.js
@@ -1,7 +1,5 @@
 ﻿$(document).on("ready", function () {
-    var menu =
-    {
-        General : [
+    var General = [
             "Cargos",
             "Empresas",
             "Idiomas",
@@ -10,81 +8,100 @@
             "Jornadas",
             "Areas",
             "TipoHoras",
+        ];
+    var Empleado =[
+                "Empleados",
+                "Sueldos",
+                "HistorialHorasTrabajadas",
+                "HistorialCargos"
+            ]
+    var ACCIONES = {
+        Amonestaciones: [
+                "TipoAmonestaciones",
+                "HistorialAmonestaciones"
         ],
-        Empleado :[
-            "Empleados",
-            "Sueldos",
-            "HistorialHorasTrabajadas",
-            "HistorialCargos"
+        Salidas: [
+                "RazonSalidas",
+                "TipoSalidas",
+                "HistorialSalidas"
+        ],
+        Permisos: [
+                "TipoPermisos",
+                "HistorialPermisos"
+        ],
+        Incapacidades: [
+                "TipoIncapacidades",
+                "HistorialPermisos"
+        ],
+        Reclutamiento: [
+                "Competencias",
+                "Habilidades",
+                "Titulos",
+                "FasesReclutamiento",
+                "Personas",
+                "SeleccionCandidatos",
+                "RequerimientosEspeciales"
         ]
-    };
-    var sub_Menu = {
-        "Acciones de Personal" :{
-                    Amonestaciones :[
-                            "TipoAmonestaciones",
-                            "HistorialAmonestaciones"
-                    ],
-                    Salidas :[
-                            "RazonSalidas",
-                            "TipoSalidas",
-                            "HistorialSalidas"
-                    ],
-                    Permisos :[
-                            "TipoPermisos",
-                            "HistorialPermisos"
-                    ],
-                    Incapacidades :[
-                            "TipoIncapacidades",
-                            "HistorialPermisos"
-                    ],
-                    Reclutamiento :[
-                            "Competencias",
-                            "Habilidades",
-                            "Titulos",
-                            "FasesReclutamiento",
-                            "Personas",
-                            "SeleccionCandidatos",
-                            "RequerimientosEspeciales"
-                    ]
-                }
     };
     var url = window.location.href;
     var pieces = url.split("/");
-    var controler=pieces[pieces.length-1]
-    //pieces = pieces[1].split("/");
-    $(".active").removeClass("active");
-    $.each(menu, function (index,value) {
-        $.each(value, function (indice, valor) {
-            if (valor == controler) {
-                $("#" + index).addClass("active");
-                $("#" + index).find("."+index).addClass("in");
-            }
-        });
-    });
-    $.each(sub_Menu, function (index, value) {
-        $.each(value, function (indice, valor) {
-            if (buscarItem(controler,valor)) {
-                $.each(valor, function (indice1, valor1) {
-                    if (valor1 == controler) {
-                        $("#ACCIONES").addClass("active");
-                        $("#ACCIONES").find(".ACCIONES").addClass("in");
+    var controler=pieces[pieces.length-1]    
+    //$.each(menu, function (index,value) {
+    //    $.each(value, function (indice, valor) {
+    //        if (valor == controler) {
+    //            $("#" + index).addClass("active");
+    //            $("#" + index).find("."+index).addClass("in");
+    //        }
+    //    });
+    //});
+    //$.each(sub_Menu, function (index, value) {
+    //    $.each(value, function (indice, valor) {
+    //        if (buscarItem(controler,valor)) {
+    //            $.each(valor, function (indice1, valor1) {
+    //                if (valor1 == controler) {
+    //                    $("#"+index).addClass("active");
+    //                    $("#" + index).find("." + index).addClass("in");
+    //                    $("#" + indice).addClass("active");
+    //                    $("#" + indice).find("." + indice).addClass("in");
+    //                    console.log(index);
+    //                    console.log(indice);
+    //                    console.log(controler);
+    //                }
+    //            });
+    //        }
+    //    });
+    //});
 
-                        $("#" + indice).addClass("active");
-                        $("#" + indice).find("." + indice).addClass("in");
-                        console.log(index);
-                        console.log(indice);
-                        console.log(controler);
-                    }
-                });
+    if (buscarItem(controler, General)) {
+        $("#General").addClass("active");
+        $("#General").find(".General").addClass("in");
+    } else if (buscarItem(controler, Empleado)) {
+        $("#General").addClass("active");
+        $("#General").find(".General").addClass("in");
+        $("#Empleado").addClass("active");
+        $("#Empleado").find(".Empleado").addClass("in");
+    } else {
+        $.each(ACCIONES, function (indice, valor) {
+            if (buscarItem(controler, valor)) {
+                $("#ACCIONES").addClass("active");
+                $("#ACCIONES").find(".ACCIONES").addClass("in");
+
+                //$("#" + index).addClass("active");
+                //$("#" + index).find("." + index).addClass("in");
+
+                $("#" + indice).addClass("active");
+                $("#" + indice).find("." + indice).addClass("in");
             }
         });
-    });
+    }
+
 });
 function buscarItem(item, lista) {
-    lista.foreach(function (indice,value) {
-        if(valor==value){
-            return true;
+    var valor = false;
+    $.each(lista, function (index, value) {
+        if (item == value) {
+            valor= true;
         }
-        return false;
     });
+    return valor;
 }
