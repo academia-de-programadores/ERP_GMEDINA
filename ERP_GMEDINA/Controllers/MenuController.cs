46c8ce73c9940d501a01533b4f0009f524a86edd
RAMT96 <arobert_17@hotmail.com>
2020-01-09 16:16:03 -0600

    Actualizacion AuxilioCesantia

--- ERP_GMEDINA/Controllers/MenuController.cs
+++ ERP_GMEDINA/Controllers/MenuController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ERP_GMEDINA.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,12 @@ namespace ERP_GMEDINA.Controllers
 {
     public class MenuController : Controller
     {
+        private ERP_GMEDINAEntities db = new ERP_GMEDINAEntities();
         // GET: Menu
         public ActionResult Index()
         {
+            tbUsuario sesionUsuario = db.tbUsuario.Where(x => x.usu_Id == 1).FirstOrDefault();
+            Session["sesionUsuario"] = sesionUsuario;
             return View();
         }
     }
