3b3e6cf5d3cd51257837754840ab139893d4ba78
Carlos Flamenco <57505181+cflamenco99@users.noreply.github.com>
2020-01-10 11:24:44 -0600

    Rotativa Reports Integration

---
+++ ERP_GMEDINA/Models/ViewModelDeduccionesReport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ERP_GMEDINA.Models
+{
+	public class ViewModelDeduccionesReport
+	{
+		public int emp_Id { get; set; }
+		public string per_Nombres { get; set; }
+		public string per_Apellidos { get; set; }
+		public int cde_IdDeducciones { get; set; }
+		public string cde_DescripcionDeduccion { get; set; }
+		public decimal? hidp_Total { get; set; }
+		public DateTime hipa_FechaInicio { get; set; }
+		public DateTime hipa_FechaFin { get; set; }
+		public string cpla_DescripcionPlanilla { get; set; }
+	}
+
+}
