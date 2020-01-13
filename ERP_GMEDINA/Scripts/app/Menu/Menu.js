17dba0ee922f2cc942470033a47978383cd2085f
AlexDiaz22 <alexleonel96@hotmail.com>
2020-01-13 11:41:30 -0600

    comit

--- ERP_GMEDINA/Scripts/app/Menu/Menu.js
+++ ERP_GMEDINA/Scripts/app/Menu/Menu.js
@@ -52,55 +52,32 @@
     };
     var url = window.location.href;
     var pieces = url.split("/");
-    var controler=pieces[pieces.length-1]    
-    //$.each(menu, function (index,value) {
-    //    $.each(value, function (indice, valor) {
-    //        if (valor == controler) {
-    //            $("#" + index).addClass("active");
-    //            $("#" + index).find("."+index).addClass("in");
-    //        }
-    //    });
-    //});
-    //$.each(sub_Menu, function (index, value) {
-    //    $.each(value, function (indice, valor) {
-    //        if (buscarItem(controler,valor)) {
-    //            $.each(valor, function (indice1, valor1) {
-    //                if (valor1 == controler) {
-    //                    $("#"+index).addClass("active");
-    //                    $("#" + index).find("." + index).addClass("in");
-    //                    $("#" + indice).addClass("active");
-    //                    $("#" + indice).find("." + indice).addClass("in");
-    //                    console.log(index);
-    //                    console.log(indice);
-    //                    console.log(controler);
-    //                }
-    //            });
-    //        }
-    //    });
-    //});
+    var controler = ''; //pieces[pieces.length - 1];
+    pieces.forEach(function (value, index) {
+        controler = value;
+        if (buscarItem(controler, General)) {
+            $("#General").addClass("active");
+            $("#General").find(".General").addClass("in");
+        } else if (buscarItem(controler, Empleado)) {
+            $("#General").addClass("active");
+            $("#General").find(".General").addClass("in");
+            $("#Empleado").addClass("active");
+            $("#Empleado").find(".Empleado").addClass("in");
+        } else {
+            $.each(ACCIONES, function (indice, valor) {
+                if (buscarItem(controler, valor)) {
+                    $("#ACCIONES").addClass("active");
+                    $("#ACCIONES").find(".ACCIONES").addClass("in");
 
-    if (buscarItem(controler, General)) {
-        $("#General").addClass("active");
-        $("#General").find(".General").addClass("in");
-    } else if (buscarItem(controler, Empleado)) {
-        $("#General").addClass("active");
-        $("#General").find(".General").addClass("in");
-        $("#Empleado").addClass("active");
-        $("#Empleado").find(".Empleado").addClass("in");
-    } else {
-        $.each(ACCIONES, function (indice, valor) {
-            if (buscarItem(controler, valor)) {
-                $("#ACCIONES").addClass("active");
-                $("#ACCIONES").find(".ACCIONES").addClass("in");
+                    //$("#" + index).addClass("active");
+                    //$("#" + index).find("." + index).addClass("in");
 
-                //$("#" + index).addClass("active");
-                //$("#" + index).find("." + index).addClass("in");
-
-                $("#" + indice).addClass("active");
-                $("#" + indice).find("." + indice).addClass("in");
-            }
-        });
-    }
+                    $("#" + indice).addClass("active");
+                    $("#" + indice).find("." + indice).addClass("in");
+                }
+            });
+        }
+    });
 
 });
 function buscarItem(item, lista) {
