b23823ca7a331523efc82ea7ed2d3c079980fd22
mauriciodiaz99 <dwdmmadm2001@gmail.com>
2020-01-14 15:52:06 -0600

    Cambios y Detalles

--- ERP_GMEDINA/Models/cAFP.cs
+++ ERP_GMEDINA/Models/cAFP.cs
@@ -18,30 +18,33 @@ namespace ERP_GMEDINA.Models
         public int afp_Id { get; set; }
 
         [StringLength(100, ErrorMessage = "No puede ingresar más de 100 caracteres")]
-        [Required(ErrorMessage = "El campo AFP es Requerido")]
+        [Required(ErrorMessage = "Campo AFP Requerido")]
         [RegularExpression(@"^[a-zA-Z ]*$", ErrorMessage = "Texto válido sin Números")]
         [Display(Name = "AFP")]
         public string afp_Descripcion { get; set; }
 
         [Range(0.01, 9999999999.99, ErrorMessage = "El Aporte no puede ser menor de 0 dígitos, ni mayor que 10 dígitos")]
-        [Required(ErrorMessage = "El campo Aporte Mínimo es Requerido")]
+        [Required(ErrorMessage = "Campo Aporte Mínimo Requerido")]
         [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Número decimal válido con un máximo de 2 decimales.")]
+        [DataType(DataType.Currency)]
         [Display(Name = "Aporte Mínimo")]
         public decimal afp_AporteMinimoLps { get; set; }
 
         [Range(0.01, 9999999999.99, ErrorMessage = "El Interés por Aporte no puede ser menor de 0 dígitos, ni mayor que 10 dígitos")]
-        [Required(ErrorMessage = "El campo Interés por Aporte es Requerido")]
+        [Required(ErrorMessage = "Campo Interés Aporte Requerido")]
         [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Número decimal válido con un máximo de 2 decimales.")]
+        [DataType(DataType.Currency)]
         [Display(Name = "Interés por Aporte")]
         public decimal afp_InteresAporte { get; set; }
 
         [Range(0.01, 9999999999.99, ErrorMessage = "El Interés Anual no puede ser menor de 0 dígitos, ni mayor que 10 dígitos")]
-        [Required(ErrorMessage = "El campo Interés Anual es Requerido")]
+        [Required(ErrorMessage = "Campo Interés Anual Requerido")]
         [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Número decimal válido con un máximo de 2 decimales.")]
+        [DataType(DataType.Currency)]
         [Display(Name = "Interés Anual")]
         public decimal afp_InteresAnual { get; set; }
 
-        [Required(ErrorMessage = "El campo Tipo Deducción es Requerido")]
+        [Required(ErrorMessage = "Campo Tipo Deducción Requerido")]
         [Display(Name = "Tipo Deducción")]
         public int tde_IdTipoDedu { get; set; }
 
--- ERP_GMEDINA/Models/cDeduccionesIndividuales.cs
+++ ERP_GMEDINA/Models/cDeduccionesIndividuales.cs
@@ -17,29 +17,32 @@ namespace ERP_GMEDINA.Models
         public int dei_IdDeduccionesIndividuales { get; set; }
 
         [StringLength(100, ErrorMessage = "No puede ingresar más de 100 caracteres")]
-        [Required(ErrorMessage = "El campo Motivo es Requerido")]
+        [Required(ErrorMessage = "Campo Motivo Requerido")]
         [Display(Name = "Motivo")]
         public string dei_Motivo { get; set; }
 
-        [Required(ErrorMessage = "El campo Empleado es requerido")]
+        [Required(ErrorMessage = "Campo Empleado Requerido")]
         [Display(Name = "Empleado")]
         public int emp_Id { get; set; }
 
         [Range(0.01, 9999999999.99, ErrorMessage = "El Monto Inicial no puede ser menor a 0 dígitos, ni mayor a 10 dígitos")]
-        [Required(ErrorMessage = "El campo Monto Inicial es Requerido")]
+        [Required(ErrorMessage = "Campo Monto Inicial Requerido")]
         [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Número decimal válido con un máximo de 2 decimales.")]
+        [DataType(DataType.Currency)]
         [Display(Name = "Monto Inicial")]
         public decimal dei_MontoInicial { get; set; }
 
         [Range(0.01, 9999999999.99, ErrorMessage = "La Cuota no puede ser menor a 0 dígitos, ni mayor a 10 dígitos")]
-        [Required(ErrorMessage = "El campo Monto Restante es Requerido")]
+        [Required(ErrorMessage = "Campo Monto Restante Requerido")]
         [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Número decimal válido con un máximo de 2 decimales.")]
+        [DataType(DataType.Currency)]
         [Display(Name = "Monto Restante")]
         public decimal dei_MontoRestante { get; set; }
 
         [Range(0.01, 9999999999.99, ErrorMessage = "El Monto Inicial no puede ser menor a 0 dígitos, ni mayor a 10 dígitos")]
-        [Required(ErrorMessage = "El campo Cuota es Requerido")]
+        [Required(ErrorMessage = "Campo Cuota Requerido")]
         [RegularExpression(@"^[0-9]+(\.[0-9]{1,2})$", ErrorMessage = "Número decimal válido con un máximo de 2 decimales.")]
+        [DataType(DataType.Currency)]
         [Display(Name = "Cuota")]
         public decimal dei_Cuota { get; set; }
 
