0bd92901a04e610c0dec4fc18ed879f8bfa33e1f
keyci1113 <47374214+keyci1113@users.noreply.github.com>
2020-01-15 16:27:43 -0600

    ARREGLO DE MENU

--- ERP_GMEDINA/Scripts/app/Menu/Menu.js
+++ ERP_GMEDINA/Scripts/app/Menu/Menu.js
@@ -9,47 +9,49 @@
             "Areas",
             "TipoHoras",
             "Jornadas"
+    ];
             
-        ];
-    var Empleado =[
-                "Empleados",
-                "Sueldos",
-                "HistorialVacaciones",
-                "HistorialHorasTrabajadas",
-                "HistorialCargos"
-                
-            ]
-    var ACCIONES = {
-   
-        Amonestaciones: [
-                "TipoAmonestaciones",
-                "HistorialAudienciaDescargos",
-                "HistorialAmonestaciones"
-        ],
-        Salidas: [
-                "RazonSalidas",
-                "TipoSalidas",
-                "HistorialSalidas"
-        ],
-        Permisos: [
-                "TipoPermisos",
-                "HistorialPermisos"
-        ],
-        Incapacidades: [
-                "TipoIncapacidades",
-                "HistorialIncapacidades"
-        ],
-        Reclutamiento: [
-                "Competencias",
-                "Habilidades",
-                "Titulos",
-                "FasesReclutamiento",
-                "Personas",
-                "Requisiciones",
-                "SeleccionCandidatos",
-                "RequerimientosEspeciales"
-        ]
-    };
+    var Empleados= [
+          "Empleados",
+            "Sueldos",
+            "HistorialVacaciones",
+            "HistorialHorasTrabajadas",
+            "HistorialCargos"
+    ];
+
+    var Amonestaciones= [
+            "TipoAmonestaciones",
+            "HistorialAudienciaDescargos",
+            "HistorialAmonestaciones"
+    ];
+    var Salidas= [
+            "RazonSalidas",
+            "TipoSalidas",
+            "HistorialSalidas"
+    ];
+    var Permisos= [
+            "TipoPermisos",
+            "HistorialPermisos"
+    ];
+    var Incapacidades= [
+            "TipoIncapacidades",
+            "HistorialIncapacidades"
+    ];
+    var Reclutamiento = [
+           
+            "Personas",
+            "Requisiciones",
+            "SeleccionCandidatos"
+           
+    ];
+    var Datos_Profesionales = [
+         "Competencias",
+            "Habilidades",
+            "Titulos",
+            "FasesReclutamiento",
+             "RequerimientosEspeciales"
+    ]
+    
     var url = window.location.href;
     var pieces = url.split("/");
     var controler = ''; //pieces[pieces.length - 1];
@@ -57,25 +59,34 @@
         controler = value;
         if (buscarItem(controler, General)) {
             $("#General").addClass("active");
-            $("#General").find(".General").addClass("in");
-        } else if (buscarItem(controler, Empleado)) {
-            $("#General").addClass("active");
-            $("#General").find(".General").addClass("in");
-            $("#Empleado").addClass("active");
-            $("#Empleado").find(".Empleado").addClass("in");
-        } else {
-            $.each(ACCIONES, function (indice, valor) {
-                if (buscarItem(controler, valor)) {
-                    $("#ACCIONES").addClass("active");
-                    $("#ACCIONES").find(".ACCIONES").addClass("in");
-
-                    //$("#" + index).addClass("active");
-                    //$("#" + index).find("." + index).addClass("in");
-
-                    $("#" + indice).addClass("active");
-                    $("#" + indice).find("." + indice).addClass("in");
-                }
-            });
+            $("#General").find(".General").addClass("in");}
+        else if (buscarItem(controler, Empleados)) {
+            $("#Empleados").addClass("active");
+            $("#Empleados").find(".Empleados").addClass("in");
+        }
+        else if (buscarItem(controler, Amonestaciones)) {
+            $("#Amonestaciones").addClass("active");
+            $("#Amonestaciones").find(".Amonestaciones").addClass("in");
+        }
+        else if (buscarItem(controler, Salidas)) {
+            $("#Salidas").addClass("active");
+            $("#Salidas").find(".Salidas").addClass("in");
+        }
+        else if (buscarItem(controler, Permisos)) {
+            $("#Permisos").addClass("active");
+            $("#Permisos").find(".Permisos").addClass("in");
+        }
+        else if (buscarItem(controler, Incapacidades)) {
+            $("#Incapacidades").addClass("active");
+            $("#Incapacidades").find(".Incapacidades").addClass("in");
+        }
+        else if (buscarItem(controler, Reclutamiento)) {
+            $("#Reclutamiento").addClass("active");
+            $("#Reclutamiento").find(".Reclutamiento").addClass("in");
+        }
+        else if (buscarItem(controler, Datos_Profesionales)) {
+            $("#Datos_Profesionales").addClass("active");
+            $("#Datos_Profesionales").find(".Datos_Profesionales").addClass("in");
         }
     });
 
