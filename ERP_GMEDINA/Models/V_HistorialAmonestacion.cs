5aeb4bb1a9eb2ef020509c633700d8a9271ded7a
Chris <Andino>
2020-01-15 07:57:54 -0600

    Correccion de modelo

--- ERP_GMEDINA/Models/V_HistorialAmonestacion.cs
+++ ERP_GMEDINA/Models/V_HistorialAmonestacion.cs
@@ -21,7 +21,6 @@ namespace ERP_GMEDINA.Models
         public string tamo_Descripcion { get; set; }
         public int hamo_Id { get; set; }
         public Nullable<System.DateTime> hamo_Fecha { get; set; }
-        public int hamo_AmonestacionAnterior { get; set; }
         public string hamo_Observacion { get; set; }
         public bool hamo_Estado { get; set; }
         public string hamo_RazonInactivo { get; set; }
--- ERP_GMEDINA/Models/tbHistorialAmonestaciones.cs
+++ ERP_GMEDINA/Models/tbHistorialAmonestaciones.cs
@@ -14,17 +14,11 @@ namespace ERP_GMEDINA.Models
     
     public partial class tbHistorialAmonestaciones
     {
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
-        public tbHistorialAmonestaciones()
-        {
-            this.tbHistorialAmonestaciones1 = new HashSet<tbHistorialAmonestaciones>();
-        }
-    
         public int hamo_Id { get; set; }
         public int emp_Id { get; set; }
         public int tamo_Id { get; set; }
         public System.DateTime hamo_Fecha { get; set; }
-        public int hamo_AmonestacionAnterior { get; set; }
+        public Nullable<int> hamo_AmonestacionAnterior { get; set; }
         public string hamo_Observacion { get; set; }
         public bool hamo_Estado { get; set; }
         public string hamo_RazonInactivo { get; set; }
@@ -36,9 +30,6 @@ namespace ERP_GMEDINA.Models
         public virtual tbUsuario tbUsuario { get; set; }
         public virtual tbUsuario tbUsuario1 { get; set; }
         public virtual tbEmpleados tbEmpleados { get; set; }
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
-        public virtual ICollection<tbHistorialAmonestaciones> tbHistorialAmonestaciones1 { get; set; }
-        public virtual tbHistorialAmonestaciones tbHistorialAmonestaciones2 { get; set; }
         public virtual tbTipoAmonestaciones tbTipoAmonestaciones { get; set; }
     }
 }
