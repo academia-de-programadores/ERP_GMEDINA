826e09c8558a74121628e17b39f0c1d357c4035b
Kevin Alemán <57551666+Kevin4leman@users.noreply.github.com>
2020-01-17 17:45:29 -0600

    xd

--- ERP_GMEDINA/Scripts/app/General/TipoMonedas/Admin.js
+++ ERP_GMEDINA/Scripts/app/General/TipoMonedas/Admin.js
@@ -1,25 +1,27 @@
 ﻿Admin = true;
 //Esta funcion llama al modal de Habilitar
 function hablilitar(btn) {
- var tr = $(btn).closest('tr');
- var row = tabla.row(tr);
- var id = row.data().ID;
- //$("#txtIdRestore").val(id);
- $("#ModalHabilitar").data("id", id);
- $('#ModalHabilitar').modal('show');
+    var tr = $(btn).closest('tr');
+    var row = tabla.row(tr);
+    var id = row.data().ID;
+    $("#txtIdRestore").val(id);
+    //console.log($("#txtIdRestore").val(id));
+    $('#ModalHabilitar').modal('show');
 }
 
 //Cambiar el controlador para ejecutar el UDP de restaurar
 $("#btnActivar").click(function () {
- var Id = $("#ModalHabilitar").data("id");
- //$("#txtIdRestore").val();
- _ajax(JSON.stringify({ id: Id }), // <<<<<<===================================
-     '/TipoMonedas/hablilitar/',
-     'POST',
-     function (obj) {
-      if (obj != "-1" && obj != "-2" && obj != "-3") {
-       llenarTabla(-1);
-      }
-     });
- CierraPopups();
+    var Id = $("#txtIdRestore").val();
+    _ajax(JSON.stringify({ id: Id }), // <<<<<<===================================
+        '/TipoPermisos/hablilitar/',
+        'POST',
+        function (obj) {
+            if (obj != "-1" && obj != "-2" && obj != "-3") {
+                llenarTabla(-1);
+                MsgSuccess("¡Exito!", "El registro se habilitado  de forma exitosa");
+            } else {
+                MsgError("Error", "No se logró habilitado el registro, contacte al administrador");
+            }
+        });
+    CierraPopups();
 });
