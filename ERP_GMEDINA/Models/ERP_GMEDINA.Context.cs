3837e0aebb84dec2043acb041ada6730036080cc
keyci1113 <47374214+keyci1113@users.noreply.github.com>
2020-01-19 05:24:35 -0600

    requisicion

--- ERP_GMEDINA/Models/ERP_GMEDINA.Context.cs
+++ ERP_GMEDINA/Models/ERP_GMEDINA.Context.cs
@@ -125,6 +125,7 @@ namespace ERP_GMEDINA.Models
         public virtual DbSet<V_RPT_HistorialSalidas> V_RPT_HistorialSalidas { get; set; }
         public virtual DbSet<V_RPT_Requisiciones> V_RPT_Requisiciones { get; set; }
         public virtual DbSet<V_RPT_HistorialSueldos> V_RPT_HistorialSueldos { get; set; }
+        public virtual DbSet<V_RPT_HistorialAmonestaciones_Empleados> V_RPT_HistorialAmonestaciones_Empleados { get; set; }
     
         public virtual int sp_alterdiagram(string diagramname, Nullable<int> owner_id, Nullable<int> version, byte[] definition)
         {
