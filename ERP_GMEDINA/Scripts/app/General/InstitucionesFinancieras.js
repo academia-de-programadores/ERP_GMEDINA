c0e0b32bb139f97ccaecddd617ea0201fcff96f5
Murcia7144 <jose.murcia@bi-dss.com>
2020-01-20 15:05:10 -0600

    Validar ahora en el JS los Ranges y Regular Expression 50/50

--- ERP_GMEDINA/Scripts/app/General/InstitucionesFinancieras.js
+++ ERP_GMEDINA/Scripts/app/General/InstitucionesFinancieras.js
@@ -90,41 +90,41 @@ function Validacion(
 
     // Descripción Institución Financiera
     if (DescripInstFin.val() != '') {
-        AsteriscoDescripcion.removeClass('text-danger');
-        validacionDescripcion.hide();
+        AsteriscDescrip.removeClass('text-danger');
+        validaDescripcion.hide();
     } else {
-        AsteriscoDescripcion.addClass('text-danger');
-        validacionDescripcion.show();
+        AsteriscDescrip.addClass('text-danger');
+        validaDescripcion.show();
         todoBien = false;
     }
 
     // Contacto
     if (Contact.val() != '') {
-        AsteriscoContacto.removeClass('text-danger');
-        validacionContacto.hide();
+        AsteriscContact.removeClass('text-danger');
+        validaContacto.hide();
     } else {
-        AsteriscoContacto.addClass('text-danger');
-        validacionContacto.show();
+        AsteriscContact.addClass('text-danger');
+        validaContacto.show();
         todoBien = false;
     }
 
     // Telefono
     if (Tel.val() != '') {
-        validacionTelefono.hide();
-        AsteriscoTelefono.removeClass('text-danger');
+        validaTelefono.hide();
+        AsteriscTel.removeClass('text-danger');
     } else {
-        AsteriscoTelefono.addClass('text-danger');
-        validacionTelefono.show();
+        AsteriscTel.addClass('text-danger');
+        validaTelefono.show();
         todoBien = false;
     }
 
     // Correo
     if (Cor.val() != '') {
-        AsteriscoCorreo.removeClass('text-danger');
-        validacionCorreo.hide();
+        AsteriscCorre.removeClass('text-danger');
+        validaCorreo.hide();
     } else {
-        AsteriscoCorreo.addClass('text-danger');
-        validacionCorreo.show();
+        AsteriscCorre.addClass('text-danger');
+        validaCorreo.show();
         todoBien = false;
     }
     return todoBien;
@@ -251,6 +251,51 @@ $('#btnCargarPlanilla').click(function ()
         });
 });
 
+
+//Crear Institución Financiera ya validado
+$(btnAgregar).click(function () {
+    console.clear();
+    if (validaciones(
+    DescripInstFin,
+    Contact,
+    Tel,
+    Cor
+    )) {
+        console.log('Paso las validaciones');
+
+        var data = $("#frmCreate").serializeArray();
+        //ENVIAR DATA AL SERVIDOR PARA EJECUTAR LA INSERCIÓN
+        $.ajax({
+            url: "/DeduccionesExtraordinarias/Create",
+            method: "POST",
+            data: data
+        }).done(function (data) {
+
+            //VALIDAR RESPUESTA OBTENIDA DEL SERVIDOR, SI LA INSERCIÓN FUE EXITOSA O HUBO ALGÚN ERROR
+            if (data != "error") {
+                document.getElementById("btnAgregar").disabled = true;
+                window.location.href = '/DeduccionesExtraordinarias/Index';
+                // Mensaje de exito cuando un registro se ha guardado bien
+                iziToast.success({
+                    title: 'Exito',
+                    message: '¡El registro se agregó de forma exitosa!',
+                });
+            }
+            else {
+                iziToast.error({
+                    title: 'Error',
+                    message: '¡No se guardó el registro, contacte al administrador!',
+                });
+            }
+        });
+
+    }
+    // Evitar PostBack en los Formularios de las Vistas Parciales de Modal
+    $("#frmCreate").submit(function (e) {
+        return false;
+    });
+    document.getElementById("btnAgregar").disabled = false;
+});
 
 
 $(document).on("click", "#btnModalActualizarINFS", function () {
