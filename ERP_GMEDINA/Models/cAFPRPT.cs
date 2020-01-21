2bec9b925d16ed5687b9443491aa100e91bf2870
Carlos Flamenco <57505181+cflamenco99@users.noreply.github.com>
2020-01-21 12:53:35 -0600

    Reportes

--- ERP_GMEDINA/Models/cAFPRPT.cs
+++ ERP_GMEDINA/Models/cAFPRPT.cs
@@ -15,31 +15,31 @@ namespace ERP_GMEDINA.Models
     public class cAFPRPT
     {
 		[Display(Name = "Codigo Empleado")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public int emp_Id { get; set; }
 
 		[Display(Name = "Nombres")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public string per_Nombres { get; set; }
 
 		[Display(Name = "Apellidos")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public string per_Apellidos { get; set; }
 
 		[Display(Name = "Total AFP")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public decimal hipa_AFP { get; set; }
 
 		[Display(Name = "Codigo Planilla")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public int cpla_IdPlanilla { get; set; }
 
 		[Display(Name = "Planilla")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public string cpla_DescripcionPlanilla { get; set; }
 
 		[Display(Name = "Fecha Pago")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public System.DateTime hipa_FechaPago { get; set; }
 
     }
--- ERP_GMEDINA/Models/cRAPRPT.cs
+++ ERP_GMEDINA/Models/cRAPRPT.cs
@@ -16,47 +16,47 @@ namespace ERP_GMEDINA.Models
     {
 
 		[Display(Name = "Codigo Empleado")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public int emp_Id { get; set; }
 
 		[Display(Name = "Nombres")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public string per_Nombres { get; set; }
 
 		[Display(Name = "Apellidos")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public string per_Apellidos { get; set; }
 
 		[Display(Name = "Codigo Deduccion")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public int cde_IdDeducciones { get; set; }
 
 		[Display(Name = "Deduccion")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public string cde_DescripcionDeduccion { get; set; }
 
 		[Display(Name = "Total a Deducir")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public Nullable<decimal> hidp_Total { get; set; }
 
 		[Display(Name = "Fecha Inicio")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public System.DateTime hipa_FechaInicio { get; set; }
 
 		[Display(Name = "Fecha Fin")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public System.DateTime hipa_FechaFin { get; set; }
 
 		[Display(Name = "Fecha Pago")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public System.DateTime hipa_FechaPago { get; set; }
 
 		[Display(Name = "Codigo Planilla")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public int cpla_IdPlanilla { get; set; }
 
 		[Display(Name = "Planilla")]
-		[Required(ErrorMessage = "No puede dejar campos vacios.")]
+		[Required(ErrorMessage = "Campo {0} requerido.")]
 		public string cpla_DescripcionPlanilla { get; set; }
 	}
 }
