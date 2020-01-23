bcc3a57f958e12bc6ea1044fbf6b728eba47cbbd
EG0782 <53062171+EG0782@users.noreply.github.com>
2020-01-23 07:23:52 -0600

    cEquipoEmpleados

---
+++ ERP_GMEDINA/Models/cEquipoEmpleados.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ERP_GMEDINA.Models
+{
+    [MetadataType(typeof(cEquipoEmpleados))]
+    public partial class tbEquipoEmpleados { }
+
+    public class cEquipoEmpleados
+    {
+        [Display(Name = "Fecha Entrega")]
+        public System.DateTime eqem_Fecha { get; set; }        
+    }
+}
