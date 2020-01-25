585b975c55a148e3c78d74ec50293fc44896201f
selvinmedina <selvinmedina0016@gmail.com>
2020-01-24 20:12:06 -0600

    bug UDP_Plani_tbInstitucionesFinancieras_Activar_Result y actualizacion del modelo

--- ERP_GMEDINA/Controllers/InstitucionesFinancierasController.cs
+++ ERP_GMEDINA/Controllers/InstitucionesFinancierasController.cs
@@ -295,7 +295,7 @@ namespace ERP_GMEDINA.Controllers
                     listINFS = db.UDP_Plani_tbInstitucionesFinancieras_Activar(id, 1, DateTime.Now);
 
                     // validar respuesta del procedimiento almacenado
-                    foreach (UDP_Plani_tbInstitucionesFinancieras_Activar1_Result Resultado in listINFS)
+                    foreach (UDP_Plani_tbInstitucionesFinancieras_Activar_Result Resultado in listINFS)
                         MensajeError = Resultado.MensajeError;
 
                     if (MensajeError.StartsWith("-1"))
