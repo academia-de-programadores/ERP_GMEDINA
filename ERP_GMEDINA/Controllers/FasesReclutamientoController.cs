8467bb69a05deeb943612378ca23e2243f329ad1
Enamorado8 <53063416+Enamorado8@users.noreply.github.com>
2020-01-26 20:54:38 -0600

    dispose Fases Reclutamiento

--- ERP_GMEDINA/Controllers/FasesReclutamientoController.cs
+++ ERP_GMEDINA/Controllers/FasesReclutamientoController.cs
@@ -222,7 +222,7 @@ namespace ERP_GMEDINA.Controllers
         }
         protected override void Dispose(bool disposing)
         {
-            if (disposing)
+            if (disposing && db != null)
             {
                 db.Dispose();
             }
