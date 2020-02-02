04b4321fab5f552ca0b8c9543da356a47374340b
selvinmedina <selvinmedina0016@gmail.com>
2020-02-01 22:23:22 -0600

    impuesto vecinal

--- ERP_GMEDINA/Controllers/PagoDeCesantiaDetalleController.cs
+++ ERP_GMEDINA/Controllers/PagoDeCesantiaDetalleController.cs
@@ -24,7 +24,7 @@ namespace ERP_GMEDINA.Controllers
 
             var model = Task.Run(() =>
             {
-                //ObtenerListaDePagoCesantia();
+                ObtenerListaDePagoCesantia();
             });
 
             var CesantiaList = db.V_tbPagoDeCesantiaDetalle.ToList();
@@ -44,40 +44,40 @@ namespace ERP_GMEDINA.Controllers
         }
 
         /////
-        //public async void ObtenerListaDePagoCesantia()
-        //{
-        //    Session["PagoCesantiaPreview"] = null;
-        //    //INICIALIZACION DEL OBJETO TIPO LISTA V_tbPagoDeCesantiaDetalle
-        //    List <V_tbPagoDeCesantiaDetalle> ModelPagoDeCesantiaDetalleList = new List<V_tbPagoDeCesantiaDetalle>();
-        //    //await Task.Run(() => {
-        //    //FECHA DE LA PETICION
-        //    using (ERP_GMEDINAEntities db2 = new ERP_GMEDINAEntities())
-        //    {
-        //        DateTime FechaPeticion = DateTime.Now;
-        //        //INICIALIZACION DEL OBJETO TIPO V_tbPagoDeCesantiaDetalle_Preview
-        //        var ListEmpleados = db2.V_tbPagoDeCesantiaDetalle_Preview.OrderBy(x => x.NombreCompleto).ToList();
-        //        //Iterador
-        //        int iter = 1;
-        //        foreach (V_tbPagoDeCesantiaDetalle_Preview item in ListEmpleados)
-        //        {
-        //            //INICIALIZACION DEL OBJETO TIPO V_tbPagoDeCesantiaDetalle
-        //            V_tbPagoDeCesantiaDetalle ModelPagoDeCesantiaDetalle = new V_tbPagoDeCesantiaDetalle();
-        //            //SETEAR LOS CAMPOS PARA MOSTRAR LA PROYECCIÓN 
-        //            ModelPagoDeCesantiaDetalle.IdCesantia = iter;
-        //            ModelPagoDeCesantiaDetalle.NoIdentidad = item.NoIdentidad;
-        //            ModelPagoDeCesantiaDetalle.NombreCompleto = item.NombreCompleto;
-        //            ModelPagoDeCesantiaDetalle.DiasPagados = (int)Liquidacion.Dias360AcumuladosCesantia(item.emp_Id, FechaPeticion);
-        //            ModelPagoDeCesantiaDetalle.ConSueldo = Liquidacion.Calculo_SalarioBrutoMasAlto(item.emp_Id);
-        //            ModelPagoDeCesantiaDetalle.TotalCesantiaColaborador = (ModelPagoDeCesantiaDetalle.ConSueldo / 30) * ModelPagoDeCesantiaDetalle.DiasPagados;
-        //            ModelPagoDeCesantiaDetalle.NoDeCuenta = item.NoDeCuenta;
-        //            ModelPagoDeCesantiaDetalleList.Add(ModelPagoDeCesantiaDetalle);
-        //            iter++;
-        //        }
-        //        Session["PagoCesantiaPreview"] = ModelPagoDeCesantiaDetalleList.ToList();
-        //    }
+        public async void ObtenerListaDePagoCesantia()
+        {
+            Session["PagoCesantiaPreview"] = null;
+            //INICIALIZACION DEL OBJETO TIPO LISTA V_tbPagoDeCesantiaDetalle
+            List<V_tbPagoDeCesantiaDetalle> ModelPagoDeCesantiaDetalleList = new List<V_tbPagoDeCesantiaDetalle>();
+            //await Task.Run(() => {
+            //FECHA DE LA PETICION
+            using (ERP_GMEDINAEntities db2 = new ERP_GMEDINAEntities())
+            {
+                DateTime FechaPeticion = DateTime.Now;
+                //INICIALIZACION DEL OBJETO TIPO V_tbPagoDeCesantiaDetalle_Preview
+                var ListEmpleados = db2.V_tbPagoDeCesantiaDetalle_Preview.OrderBy(x => x.NombreCompleto).ToList();
+                //Iterador
+                int iter = 1;
+                foreach (V_tbPagoDeCesantiaDetalle_Preview item in ListEmpleados)
+                {
+                    //INICIALIZACION DEL OBJETO TIPO V_tbPagoDeCesantiaDetalle
+                    V_tbPagoDeCesantiaDetalle ModelPagoDeCesantiaDetalle = new V_tbPagoDeCesantiaDetalle();
+                    //SETEAR LOS CAMPOS PARA MOSTRAR LA PROYECCIÓN 
+                    ModelPagoDeCesantiaDetalle.IdCesantia = iter;
+                    ModelPagoDeCesantiaDetalle.NoIdentidad = item.NoIdentidad;
+                    ModelPagoDeCesantiaDetalle.NombreCompleto = item.NombreCompleto;
+                    ModelPagoDeCesantiaDetalle.DiasPagados = (int)Liquidacion.Dias360AcumuladosCesantia(item.emp_Id, FechaPeticion);
+                    ModelPagoDeCesantiaDetalle.ConSueldo = Liquidacion.Calculo_SalarioBrutoMasAlto(item.emp_Id);
+                    ModelPagoDeCesantiaDetalle.TotalCesantiaColaborador = (ModelPagoDeCesantiaDetalle.ConSueldo / 30) * ModelPagoDeCesantiaDetalle.DiasPagados;
+                    ModelPagoDeCesantiaDetalle.NoDeCuenta = item.NoDeCuenta;
+                    ModelPagoDeCesantiaDetalleList.Add(ModelPagoDeCesantiaDetalle);
+                    iter++;
+                }
+                Session["PagoCesantiaPreview"] = ModelPagoDeCesantiaDetalleList.ToList();
+            }
 
-        //    //});
-        //    //return ModelPagoDeCesantiaDetalleList;
-        //}
+            //});
+            //return ModelPagoDeCesantiaDetalleList;
+        }
     }
 }
