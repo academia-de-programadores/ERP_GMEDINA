2fc87fe29dc435865e615df81a83ac00bd72322e
Murcia7144 <jose.murcia@bi-dss.com>
2020-02-06 16:45:21 -0600

    Controllers Preaviso y TechosComisiones

--- ERP_GMEDINA/Controllers/TechosComisionesController.cs
+++ ERP_GMEDINA/Controllers/TechosComisionesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ERP_GMEDINA.Attribute;
+using ERP_GMEDINA.Models;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
@@ -6,8 +8,6 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using ERP_GMEDINA.Models;
-using ERP_GMEDINA.Attribute;
 namespace ERP_GMEDINA.Controllers
 {
     public class TechosComisionesController : Controller
