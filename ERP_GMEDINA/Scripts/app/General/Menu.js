8b973d82b6c3ab5a73d15c92947eeba9e6cb09a6
WillianDiaz <willian1997.wd@gmail.com>
2020-02-06 14:18:38 -0600

    validaciones

---
+++ ERP_GMEDINA/Scripts/app/General/Menu.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+
+    debugger;
+
+    // recuperar información del usuario para las validaciones de permisos
+    $.ajax({
+        url: "/Login/LoadUserModelState",
+        method: "GET"        
+    }).done(function (data) {
+
+        console.log(data);
+
+        // validar respuesta del servidor
+        if (data == '') {
+            // mensaje de error
+            iziToast.error({
+                title: 'Error',
+                message: 'No guardó el registro, contacte al administrador',
+            });
+        }
+        else {
+            sessionStorage.setItem("VM_ModelState", data);
+
+            iziToast.success({
+                title: 'Exito',
+                message: '¡El parece que funcionó!',
+            });
+        }
+    });
+});
