61edb69e12cf4149c6aae37cb92d934c43020264
umanzorc <42988955+umanzorc@users.noreply.github.com>
2020-02-06 23:39:10 -0600

    Seguridad

--- ERP_GMEDINA/Scripts/app/General/SeleccionCandidatos/Admin.js
+++ ERP_GMEDINA/Scripts/app/General/SeleccionCandidatos/Admin.js
@@ -2,7 +2,7 @@
 
 //Esta funcion llama al modal de Habilitar
 function hablilitar(btn) {
-    var validacionPermiso = useModelState("SeleccionCandidatos/hablilitar");
+    var validacionPermiso = userModelState("SeleccionCandidatos/hablilitar");
     if (validacionPermiso.status == true) {
         var tr = $(btn).closest('tr');
         var row = tabla.row(tr);
