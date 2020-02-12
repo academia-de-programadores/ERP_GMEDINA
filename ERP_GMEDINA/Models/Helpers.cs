4ddb34c3f574e04cc47807dcadceef8b4c199668
keyci1113 <47374214+keyci1113@users.noreply.github.com>
2020-02-12 09:43:15 -0600

    c

--- ERP_GMEDINA/Models/Helpers.cs
+++ ERP_GMEDINA/Models/Helpers.cs
@@ -62,6 +62,92 @@ namespace ERP_GMEDINA.Models
             return Retorno;
         }
 
+        public bool GetRol()
+        {
+            bool state = false;
+            bool EsAdmin = false;
+            int Rol = 0;
+            try
+            {
+                Rol = (int)HttpContext.Current.Session["UserRol"];
+                EsAdmin = (bool)HttpContext.Current.Session["UserLoginEsAdmin"];
+                if (EsAdmin)
+                    state = true;
+                else
+                {
+                    if (Rol != 0)
+                        state = true;
+                }
+            }
+            catch (Exception Ex)
+            {
+                Ex.Message.ToString();
+                state = false;
+            }
+            return state;
+        }
+
+        public bool Sesiones(string sPantalla)
+        {
+            int UserID = 0;
+            bool Retorno = false;
+            byte Sesion = 0;
+
+            try
+            {
+                UserID = (int)HttpContext.Current.Session["UserLogin"];
+                Sesion = (byte)HttpContext.Current.Session["UserLoginSesion"];
+                if (Sesion > 1)
+                    Retorno = true;
+                //else
+                //{
+                //    var list = (IEnumerable<SDP_Acce_GetUserRols_Result>)HttpContext.Current.Session["UserLoginRols"];
+                //    var BuscarList = list.Where(x => x.obj_Referencia == sPantalla);
+                //    int Conteo = BuscarList.Count();
+                //    if (Conteo > 0)
+                //        Retorno = true;
+                //}
+            }
+            catch (Exception Ex)
+            {
+                Ex.Message.ToString();
+                Retorno = false;
+            }
+            return Retorno;
+        }
+
+        public bool GetUserRols(string sPantalla)
+        {
+            int UserID = 0;
+            bool EsAdmin = false;
+            bool Retorno = false;
+
+            try
+            {
+                UserID = (int)HttpContext.Current.Session["UserLogin"];
+                EsAdmin = (bool)HttpContext.Current.Session["UserLoginEsAdmin"];
+                if (EsAdmin)
+                {
+                    Retorno = true;
+                }
+                else
+                {
+                    var list = (IEnumerable<SDP_Acce_GetUserRols_Result>)HttpContext.Current.Session["UserLoginRols"];
+                    var BuscarList = list.Where(x => x.obj_Referencia == sPantalla);
+                    int Conteo = BuscarList.Count();
+                    if (Conteo > 0)
+                        Retorno = true;
+                }
+            }
+            catch (Exception Ex)
+            {
+                Ex.Message.ToString();
+                Retorno = false;
+            }
+            return Retorno;
+
+        }
+
         public void ValidarUsuario(string sPantalla, out int SesionesValidas, out bool UsuarioEstado, out bool EsAdmin, out int UsuarioRol, out bool AccesoPantalla)
         {
             UsuarioEstado = false;
