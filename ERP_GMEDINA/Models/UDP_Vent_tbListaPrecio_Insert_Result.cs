104bdc33d3a0f4fe29d1a624ba5c6530dc27e15f
RobertoToro <arobert_17@hotmail.com>
2020-02-14 14:45:41 -0600

    Creación TipoComplejos Faltantes

---
+++ ERP_GMEDINA/Models/UDP_Vent_tbListaPrecio_Insert_Result.cs
@@ -0,0 +1,10 @@
+
+namespace ERP_GMEDINA.Models
+{
+    using System;
+    
+    public partial class UDP_Vent_tbListaPrecio_Insert_Result
+    {
+        public string MensajeError { get; set; }
+    }
+}
