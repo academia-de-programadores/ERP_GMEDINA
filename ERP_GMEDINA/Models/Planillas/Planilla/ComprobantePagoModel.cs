f2243558557d143040dd5a9ffa049cc01b9deadf
selvinmedina <selvinmedina0016@gmail.com>
2020-02-15 15:06:25 -0600

    Fallo de compilacion

--- ERP_GMEDINA/Models/Planillas/Planilla/ComprobantePagoModel.cs
+++ ERP_GMEDINA/Models/Planillas/Planilla/ComprobantePagoModel.cs
@@ -47,10 +47,10 @@ namespace ERP_GMEDINA.Models
         public string concepto { get; set; }
     }
 
-    public class ViewModelTasasDeCambio
-    {
-        public int tmon_Id { get; set; }
-        public string tmon_Descripcion { get; set; }
-        public decimal tmon_Cambio { get; set; }
-    }
+    //public class ViewModelTasasDeCambio
+    //{
+    //    public int tmon_Id { get; set; }
+    //    public string tmon_Descripcion { get; set; }
+    //    public decimal tmon_Cambio { get; set; }
+    //}
 }
