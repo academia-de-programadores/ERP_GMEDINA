d050a65c3267acfabde694c7c568f2c1548ef2a0
WillianDiaz <willian1997.wd@gmail.com>
2020-02-15 18:18:28 -0600

    cambio

--- ERP_GMEDINA/Models/Planillas/Planilla/ComprobantePagoModel.cs
+++ ERP_GMEDINA/Models/Planillas/Planilla/ComprobantePagoModel.cs
@@ -46,11 +46,4 @@ namespace ERP_GMEDINA.Models
 
         public string concepto { get; set; }
     }
-
-    public class ViewModelTasasDeCambio
-    {
-        public int tmon_Id { get; set; }
-        public string tmon_Descripcion { get; set; }
-        public decimal tmon_Cambio { get; set; }
-    }
 }
