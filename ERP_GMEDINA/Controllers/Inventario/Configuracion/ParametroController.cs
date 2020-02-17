48a01db735a12b69dd3a52ae24ae69b6e6002b63
selvinmedina <selvinmedina0016@gmail.com>
2020-02-17 08:08:11 -0600

    Actualizacion del modelo

--- ERP_GMEDINA/Controllers/Inventario/Configuracion/ParametroController.cs
+++ ERP_GMEDINA/Controllers/Inventario/Configuracion/ParametroController.cs
@@ -329,13 +329,3 @@ namespace ERP_GMEDINA.Controllers
         }
     }
 }
-
-namespace ERP_GMEDINA.Models
-{
-    using System;
-
-    public partial class ConteoParametro_Result
-    {
-        public int Conteo { get; set; }
-    }
-}
